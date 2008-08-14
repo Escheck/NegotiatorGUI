e16b4a980b845eea3abe5a432f00c58c66f030ae
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2008-08-14 09:28:29 +0000

    corrected XML annotations. ArrayList loading still does not work (Dmytro works on that)

--- src/negotiator/repository/DomainRepItem.java
+++ src/negotiator/repository/DomainRepItem.java
@@ -1,17 +1,28 @@
 package negotiator.repository;
 
 import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
 /**
  * A DomainRepItem is a domain reference that can be put in the domain repository.
  * It contains only a unique reference to an xml file with the domain description.
  * @author wouter
  *
  */
+@XmlRootElement
 public class DomainRepItem implements RepItem
-{
+{	
+	@XmlAttribute
 	String fileName;
+	@XmlElementWrapper
+	@XmlAnyElement
 	ArrayList<ProfileRepItem> profiles=new ArrayList<ProfileRepItem>(); //default to empty profiles.
-	
+
+	public DomainRepItem() { fileName="unknownfilename"; }
+
 	public DomainRepItem(String file) {
 		fileName=file;
 	}
