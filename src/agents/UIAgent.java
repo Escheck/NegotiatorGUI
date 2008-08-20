9bfcbc2e0041f6e61ab6a4ffc5088b5d4d601a4e
alina <alina@906596ab-7e2f-0410-9379-aac80b4bceee>
2008-08-20 12:45:24 +0000

    The class is now keeping an Arraylist of the previous bids in order to show the bidding history in the EnterBidDialog.

--- src/agents/UIAgent.java
+++ src/agents/UIAgent.java
@@ -10,16 +10,19 @@
 package agents;
 
 import javax.swing.JOptionPane;
+import javax.swing.*;
+
+import java.awt.*;
 
 import negotiator.Agent;
 import negotiator.Bid;
-import negotiator.Domain;
+//import negotiator.Domain;
 import negotiator.actions.Accept;
 import negotiator.actions.Action;
 import negotiator.actions.EndNegotiation;
 import negotiator.actions.Offer;
 import negotiator.utility.UtilitySpace;
-import java.util.Date;
+import java.util.*;
 
 /**
  *
@@ -29,6 +32,14 @@ public class UIAgent extends Agent{
     private Action opponentAction=null;
     private EnterBidDialog ui=null;
     private Bid myPreviousBid=null;
+    
+    //Alina added...
+    private Bid oppPreviousBid=null;
+    protected int bidCounter = 0;
+    protected NegoRoundData roundData;
+    protected ArrayList<NegoRoundData> historyOfBids = null;
+    protected UtilitySpace utilSpace;
+    
     /** Creates a new instance of UIAgent */
     
     
@@ -39,14 +50,29 @@ public class UIAgent extends Agent{
  
     public void init(int sessionNumber, int sessionTotalNumber, Date startT, Integer totalTimeP, UtilitySpace us)
     {
-    	System.out.println("init UIAgent");
+    	
+    	System.out.println("try to init UIAgent");
+    	utilSpace =us;
+    	System.out.println("Utility Space initialized: "+utilSpace);
+    	historyOfBids = new ArrayList<NegoRoundData>();
         super.init (sessionNumber, sessionTotalNumber, startT,totalTimeP, us);
         System.out.println("closing old dialog of ");
         if (ui!=null) { ui.dispose(); ui=null; }
         System.out.println("old  dialog closed. Trying to open new dialog. ");
-        try { ui = new EnterBidDialog(this, null, true,us); }
-        catch (Exception e) {System.out.println("Problem in UIAgent2.init:"+e.getMessage()); e.printStackTrace(); }
-        System.out.println("finished init of UIAgent2");
+        try { 
+        	ui = new EnterBidDialog(this, null, true,us); 
+        	//alina: dialog in the center- doesnt really work  
+        	Toolkit t = Toolkit.getDefaultToolkit();
+    		int x = (int)((t.getScreenSize().getWidth() - ui.getWidth()) / 2);
+    		int y = (int)((t.getScreenSize().getHeight() - ui.getHeight()) / 2);
+    		ui.setLocation(x, y);
+        	System.out.println("finished init of UIAgent2");
+        }
+        catch (Exception e) {
+        	System.out.println("Problem in UIAgent2.init:"+e.getMessage()); e.printStackTrace(); 
+        	System.out.println("UIAgent could not be initialized");
+        }
+        
     }
 
     public void ReceiveMessage(Action opponentAction) {
@@ -62,12 +88,50 @@ public class UIAgent extends Agent{
     
     public Action chooseAction() {
         Action action = ui.askUserForAction(opponentAction, myPreviousBid);
-        if((action != null)&&(action instanceof Offer)) myPreviousBid=((Offer)action).getBid(); 
+        if((action != null)&&(action instanceof Offer)) {
+        	myPreviousBid=((Offer)action).getBid(); 
+        	if(opponentAction!=null) {
+        		oppPreviousBid = ((Offer)opponentAction).getBid();
+            	roundData = new NegoRoundData(oppPreviousBid,myPreviousBid);
+            	historyOfBids.add(roundData);
+        	}
+        	//does this happen only the first time?
+        	else{
+        		roundData = new NegoRoundData(null,myPreviousBid);
+            	historyOfBids.add(roundData);
+        	}
+        	bidCounter++;
+        }
+        
         return action;
     }
-
-
-  
     public boolean isUIAgent() { return true; }
-
+    
+    public Bid getMyPreviousBid(){
+    	return myPreviousBid;
+    }
+    public Bid getOppPreviousBid(){
+    	return oppPreviousBid;
+    }
+   
 }
+
+class NegoRoundData{
+	private Bid lastOppBid;
+	private Bid ourLastBid;
+		
+	public NegoRoundData (Bid lastOppBid, Bid ourLastBid){
+		this.lastOppBid = lastOppBid;
+		this.ourLastBid = ourLastBid;
+	}	
+	
+	public Bid getOppentBid (){
+		return lastOppBid;
+	}
+	
+	public Bid getOurBid (){
+		return ourLastBid;
+	}
+	
+}
+
