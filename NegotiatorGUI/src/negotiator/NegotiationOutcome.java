416e7aa146e989d1c83f1bb771b286d63e7444b1
tim <tim@906596ab-7e2f-0410-9379-aac80b4bceee>
2010-11-19 14:25:07 +0000

    Logging

--- NegotiatorGUI/src/negotiator/NegotiationOutcome.java
+++ NegotiatorGUI/src/negotiator/NegotiationOutcome.java
@@ -10,8 +10,8 @@
 package negotiator;
 
 import java.util.ArrayList;
-import negotiator.analysis.BidPoint;
 
+import negotiator.analysis.BidPoint;
 import negotiator.xml.SimpleElement;
 
 /**
@@ -19,125 +19,129 @@ import negotiator.xml.SimpleElement;
  * @author dmytro
  */
 public class NegotiationOutcome {
-    public int sessionNumber;
-    public String agentAname; 	// the name of the agent
-    public String agentBname;
-    public String agentAclass;	// the class file cotnaining that agent.
-    public String agentBclass; 
-    public Double agentAutility;
-    public Double agentButility;
-    public Double agentAutilityDiscount;
-    public Double agentButilityDiscount;
-    public String ErrorRemarks; // non-null if something happens crashing the negotiation
-    public ArrayListXML<BidPoint> AgentABids;
-    public ArrayListXML<BidPoint> AgentBBids;
-    public Double agentAmaxUtil;
-    public Double agentBmaxUtil;
-    public boolean agentAstarts; // true if A starts, false if B starts
-    	// IMPORTANT. Note that this boolean differs from the agentAStarts flag that indicates whether
-    	// agent A is FORCED to use as starting agent. agentA may still be chosen to start if
-    	// that flag is not set.
-    public String agentAutilSpaceName;
-    public String agentButilSpaceName;
-    public SimpleElement additional;
+	public int sessionNumber;
+	public String agentAname; 	// the name of the agent
+	public String agentBname;
+	public String agentAclass;	// the class file cotnaining that agent.
+	public String agentBclass; 
+	public Double agentAutility;
+	public Double agentButility;
+	public Double agentAutilityDiscount;
+	public Double agentButilityDiscount;
+	public String ErrorRemarks; // non-null if something happens crashing the negotiation
+	public ArrayListXML<BidPoint> AgentABids;
+	public ArrayListXML<BidPoint> AgentBBids;
+	public Double agentAmaxUtil;
+	public Double agentBmaxUtil;
+	public boolean agentAstarts; // true if A starts, false if B starts
+	// IMPORTANT. Note that this boolean differs from the agentAStarts flag that indicates whether
+	// agent A is FORCED to use as starting agent. agentA may still be chosen to start if
+	// that flag is not set.
+	public String agentAutilSpaceName;
+	public String agentButilSpaceName;
+	public SimpleElement additional;
 
-    /** Creates a new instance of NegotiationOutcome 
-     * @param utilBDiscount 
-     * @param utilADiscount */
-    public NegotiationOutcome(int sessionNumber, 
-                    String agentAname,
-                    String agentBname,
-                    String agentAclass,
-                    String agentBclass,
-                    Double agentAutility,
-                    Double agentButility,
-                    Double agentAutilityDiscount,
-                    Double agentButilityDiscount,
-                    String err,
-                    ArrayList<BidPoint> AgentABidsP,
-                    ArrayList<BidPoint> AgentBBidsP,
-                    Double agentAmaxUtilP,
-                    Double agentBmaxUtilP,
-                    boolean startingWithA, // true if A starts, false if B starts
-                    String agentAutilSpaceNameP,
-                    String agentButilSpaceNameP,
-                    SimpleElement additional
-                    ) 
-    {
-        this.sessionNumber = sessionNumber;
-        this.agentAutility = agentAutility;
-        this.agentButility = agentButility;
-        this.agentAutilityDiscount = agentAutilityDiscount;
-        this.agentButilityDiscount = agentButilityDiscount;
-        this.agentAname = agentAname;
-        this.agentBname = agentBname;
-        this.agentAclass=agentAclass;
-        this.agentBclass=agentBclass;
-        this.additional = additional;
-        AgentABids=new ArrayListXML<BidPoint>(AgentABidsP);
-        AgentBBids=new ArrayListXML<BidPoint>(AgentBBidsP);
-        ErrorRemarks=err;
-        agentAmaxUtil=agentAmaxUtilP;
-        agentBmaxUtil=agentBmaxUtilP;
-        agentAstarts=startingWithA;
-        agentAutilSpaceName=agentAutilSpaceNameP;
-        agentButilSpaceName=agentButilSpaceNameP;
-    }
-    
-    
-    public String toString() {
-    	String startingagent="agentB"; if (agentAstarts) startingagent="agentA";
-        return String.valueOf(sessionNumber) + " agentAName="+agentAname + " agentBName=" + agentBname + 
-        " agentAutility="+agentAutility+ " agentButility="+agentButility+
-        " agentAutilityDiscount="+agentAutilityDiscount+ " agentButilityDiscount="+agentButilityDiscount+
-        " errors='"+ErrorRemarks+"'"+
-        " agentAmaxUtil="+agentAmaxUtil+ " agentBmaxUtil="+agentBmaxUtil+
-        " startingAgent="+startingagent+
-        " agentAutilspacefilename="+agentAutilSpaceName+
-        " agentButilspacefilename="+agentButilSpaceName +
-        " agentAbids="+AgentABids+" agentBbids="+AgentBBids
-        		;
+	/** Creates a new instance of NegotiationOutcome 
+	 * @param utilBDiscount 
+	 * @param utilADiscount */
+	public NegotiationOutcome(int sessionNumber, 
+			String agentAname,
+			String agentBname,
+			String agentAclass,
+			String agentBclass,
+			Double agentAutility,
+			Double agentButility,
+			Double agentAutilityDiscount,
+			Double agentButilityDiscount,
+			String err,
+			ArrayList<BidPoint> AgentABidsP,
+			ArrayList<BidPoint> AgentBBidsP,
+			Double agentAmaxUtilP,
+			Double agentBmaxUtilP,
+			boolean startingWithA, // true if A starts, false if B starts
+			String agentAutilSpaceNameP,
+			String agentButilSpaceNameP,
+			SimpleElement additional
+	) 
+	{
+		this.sessionNumber = sessionNumber;
+		this.agentAutility = agentAutility;
+		this.agentButility = agentButility;
+		this.agentAutilityDiscount = agentAutilityDiscount;
+		this.agentButilityDiscount = agentButilityDiscount;
+		this.agentAname = agentAname;
+		this.agentBname = agentBname;
+		this.agentAclass=agentAclass;
+		this.agentBclass=agentBclass;
+		this.additional = additional;
+		AgentABids=new ArrayListXML<BidPoint>(AgentABidsP);
+		AgentBBids=new ArrayListXML<BidPoint>(AgentBBidsP);
+		ErrorRemarks=err;
+		agentAmaxUtil=agentAmaxUtilP;
+		agentBmaxUtil=agentBmaxUtilP;
+		agentAstarts=startingWithA;
+		agentAutilSpaceName=agentAutilSpaceNameP;
+		agentButilSpaceName=agentButilSpaceNameP;
+	}
 
-    }
 
-    /**
-     * 
-     * @param agentX is "A" or "B"
-     * @param agentName is the given name to that agent.
-     * @param utilspacefilename is the filename holding the utility.xml file
-     * @param bids is the arraylist of bids made by that agent.
-     * @return
-     */
-    SimpleElement resultsOfAgent(String agentX,String agentName, String agentClass, String utilspacefilename,
-    		Double agentAUtil,Double agentAUtilDiscount,Double agentAMaxUtil, ArrayListXML<BidPoint> bids)
-    {
-    	SimpleElement outcome=new SimpleElement("resultsOfAgent");
-    	outcome.setAttribute("agent", agentX);
-    	outcome.setAttribute("agentName", agentName);
-    	outcome.setAttribute("agentClass", agentClass);
-    	outcome.setAttribute("utilspace", utilspacefilename);
-    	outcome.setAttribute("finalUtility",""+agentAUtil);
-    	outcome.setAttribute("discountedUtility",""+agentAUtilDiscount);
-    	outcome.setAttribute("maxUtility",""+agentAMaxUtil);
-    	Double normalized=0.; if (agentAMaxUtil>0) { normalized = agentAUtil/agentAMaxUtil; }
-    	outcome.setAttribute("normalizedUtility",""+normalized);
+	public String toString() {
+		String startingagent="agentB"; if (agentAstarts) startingagent="agentA";
+		return String.valueOf(sessionNumber) + " agentAName="+agentAname + " agentBName=" + agentBname + 
+		" agentAutility="+agentAutility+ " agentButility="+agentButility+
+		" agentAutilityDiscount="+agentAutilityDiscount+ " agentButilityDiscount="+agentButilityDiscount+
+		" errors='"+ErrorRemarks+"'"+
+		" agentAmaxUtil="+agentAmaxUtil+ " agentBmaxUtil="+agentBmaxUtil+
+		" startingAgent="+startingagent+
+		" agentAutilspacefilename="+agentAutilSpaceName+
+		" agentButilspacefilename="+agentButilSpaceName +
+		" agentAbids="+AgentABids+" agentBbids="+AgentBBids
+		;
 
-    	outcome.addChildElement(bids.toXML());
-    	return outcome;
-    }
-    
-    public SimpleElement toXML()
-    {
-    	SimpleElement outcome = new SimpleElement("NegotiationOutcome");
-    	outcome.setAttribute("currentTime", ""+Global.getCurrentTime());
-    	outcome.setAttribute("errors",ErrorRemarks);
-    	String startingagent="agentB"; if (agentAstarts) startingagent="agentA";
-    	outcome.setAttribute("startingAgent",startingagent);
-/*    	outcome.addChildElement(resultsOfAgent("A",agentAname,agentAclass,agentAutilSpaceName,
-    			agentAutility,agentAutilityDiscount,agentAmaxUtil,AgentABids));
-    	outcome.addChildElement(resultsOfAgent("B",agentBname,agentBclass,agentButilSpaceName,
-    			agentButility,agentButilityDiscount,agentBmaxUtil,AgentBBids));*/
-    	if(additional!=null) outcome.addChildElement(additional);
+	}
+
+	/**
+	 * 
+	 * @param agentX is "A" or "B"
+	 * @param agentName is the given name to that agent.
+	 * @param utilspacefilename is the filename holding the utility.xml file
+	 * @param bids is the arraylist of bids made by that agent.
+	 * @return
+	 */
+	SimpleElement resultsOfAgent(String agentX,String agentName, String agentClass, String utilspacefilename,
+			Double agentAUtil,Double agentAUtilDiscount,Double agentAMaxUtil, ArrayListXML<BidPoint> bids, boolean addBids)
+	{
+		SimpleElement outcome=new SimpleElement("resultsOfAgent");
+		outcome.setAttribute("agent", agentX);
+		outcome.setAttribute("agentName", agentName);
+		outcome.setAttribute("agentClass", agentClass);
+		outcome.setAttribute("utilspace", utilspacefilename);
+		outcome.setAttribute("finalUtility",""+agentAUtil);
+		outcome.setAttribute("discountedUtility",""+agentAUtilDiscount);
+		outcome.setAttribute("maxUtility",""+agentAMaxUtil);
+		Double normalized=0.; if (agentAMaxUtil>0) { normalized = agentAUtil/agentAMaxUtil; }
+		outcome.setAttribute("normalizedUtility",""+normalized);
+
+		if (addBids)
+			outcome.addChildElement(bids.toXML());
 		return outcome;
-    }
+	}
+
+	public SimpleElement toXML()
+	{
+		SimpleElement outcome = new SimpleElement("NegotiationOutcome");
+		outcome.setAttribute("currentTime", ""+Global.getCurrentTime());
+		outcome.setAttribute("errors",ErrorRemarks);
+		String startingagent="agentB"; if (agentAstarts) startingagent="agentA";
+		outcome.setAttribute("startingAgent",startingagent);
+		outcome.setAttribute("",startingagent);
+		
+		boolean addBids = Global.SHOW_BID_HISTORY_IN_OUTCOMES;
+		outcome.addChildElement(resultsOfAgent("A",agentAname,agentAclass,agentAutilSpaceName,
+				agentAutility,agentAutilityDiscount,agentAmaxUtil,AgentABids, addBids));
+		outcome.addChildElement(resultsOfAgent("B",agentBname,agentBclass,agentButilSpaceName,
+				agentButility,agentButilityDiscount,agentBmaxUtil,AgentBBids, addBids));
+		if(additional!=null) outcome.addChildElement(additional);
+		return outcome;
+	}
 }
