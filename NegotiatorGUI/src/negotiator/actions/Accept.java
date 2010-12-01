657441a5f471b3883c4c3e6d7a13e7b1ec81f3af
tim <tim@906596ab-7e2f-0410-9379-aac80b4bceee>
2010-12-01 09:25:51 +0000

    Offer fixes

--- NegotiatorGUI/src/negotiator/actions/Accept.java
+++ NegotiatorGUI/src/negotiator/actions/Accept.java
@@ -11,8 +11,8 @@ package negotiator.actions;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import negotiator.Agent;
 import negotiator.AgentID;
-import negotiator.Bid;
 /**
  *
  * @author Dmytro Tykhonov
@@ -27,6 +27,12 @@ public class Accept extends Action {
     public Accept(AgentID agent) {
         super(agent);
     }
+    
+    /** Creates a new instance of AcceptBid */
+    public Accept(Agent agent) {
+        this(agent.getAgentID());
+    }
+    
     //public Bid getBid() {  return fBid; }
 
     public String toString() {
