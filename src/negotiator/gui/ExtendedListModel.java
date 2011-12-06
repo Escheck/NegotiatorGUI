83d30fb0ca699794b2c250c49422efb2e8e23233
mark <mark@906596ab-7e2f-0410-9379-aac80b4bceee>
2011-12-06 10:49:52 +0000

    Fix for java 1.6 support

--- src/negotiator/gui/ExtendedListModel.java
+++ src/negotiator/gui/ExtendedListModel.java
@@ -11,7 +11,7 @@ import javax.swing.AbstractListModel;
  * @author Mark Hendrikx (m.j.c.hendrikx@student.tudelft.nl)
  * @version 05/12/11
  */
-public class ExtendedListModel<A> extends AbstractListModel<A> {
+public class ExtendedListModel<A> extends AbstractListModel {
 	private static final long serialVersionUID = -8345719619830961700L;
 	ArrayList<A> items = new ArrayList<A>();
 
