8926ace5bf8d8fc8f73bde7583bb792a7aa7670b
crw104 <crw104@906596ab-7e2f-0410-9379-aac80b4bceee>
2012-01-06 19:24:06 +0000

    Added handling of -a (agents), -p (profiles), -d (domain) and -f (output file). Note that these options are not currently used by the GUI.

--- src/misc/CommandLineOptions.java
+++ src/misc/CommandLineOptions.java
@@ -1,5 +1,7 @@
 package misc;
 
+import java.util.List;
+
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
@@ -12,6 +14,11 @@ public class CommandLineOptions
 	public boolean startTournament = false;
 	/** Automatically quit after the tournament */
 	public boolean quitWhenTournamentDone = false;
+	public List<String> agents;
+	public List<String> profiles;
+	public String protocol = "negotiator.protocol.alternatingoffers.AlternatingOffersProtocol";
+	public String domain;
+	public String outputFile;
 	
 	public static void main(String[] args)
 	{
@@ -22,7 +29,7 @@ public class CommandLineOptions
 
 	public void parse(String [] args)
 	{
-		OptionParser parser = new OptionParser( "stq:q::" );
+		OptionParser parser = new OptionParser( "stq:q::a:b:p:d:f:w:" );
 
         OptionSet options = parser.parse(args);
 
@@ -32,7 +39,15 @@ public class CommandLineOptions
         	newTournament = true;
         if (options.has("q"))
         	quitWhenTournamentDone = true;
-        
+        if (options.has("a"))
+        	agents = (List<String>) options.valuesOf("a");
+        if (options.has("p"))
+        	profiles = (List<String>) options.valuesOf("p");
+        if (options.has("d"))
+        	domain = (String) options.valueOf("d");
+        if (options.has("f"))
+        	outputFile = (String) options.valueOf("f");
+                
 //        System.out.println( options.has( "c" ) );
 //        System.out.println( options.hasArgument( "c" ) );
 //        System.out.println( options.valueOf( "c" ) );
