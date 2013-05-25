d4bd5e3eea647aab94f6e1312bddb49231879cd2
alex <alex@906596ab-7e2f-0410-9379-aac80b4bceee>
2013-05-25 17:48:29 +0000

    ANAC2013 Agents Added

---
+++ NegotiatorGUI/src/agents/anac/y2013/MetaAgent/portfolio/thenegotiatorreloaded/UtilitySpaceAdapter.java
@@ -0,0 +1,26 @@
+package agents.anac.y2013.MetaAgent.portfolio.thenegotiatorreloaded;
+
+import negotiator.Bid;
+import negotiator.Domain;
+import negotiator.utility.UtilitySpace;
+
+public class UtilitySpaceAdapter extends UtilitySpace {
+	
+	private OpponentModel opponentModel;
+		
+	public UtilitySpaceAdapter(OpponentModel opponentModel, Domain domain) {
+		this.opponentModel = opponentModel;
+		this.domain = domain;
+	}
+	
+	public double getUtility(Bid b)
+	{ 
+		double u=0.;
+		try { u = opponentModel.getBidEvaluation(b); } 
+		catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("getNormalizedUtility failed. returning 0");u=0.;}
+		return u;
+	} 
+}
+
