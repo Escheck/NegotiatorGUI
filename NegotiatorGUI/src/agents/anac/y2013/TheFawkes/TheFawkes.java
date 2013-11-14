57f03330eb4403478095ccf8fd34a79f68367dee
mark <mark@906596ab-7e2f-0410-9379-aac80b4bceee>
2013-11-14 19:09:05 +0000

    ANAC2013 versions of InoxAgent TheFawkes

---
+++ NegotiatorGUI/src/agents/anac/y2013/TheFawkes/TheFawkes.java
@@ -0,0 +1,30 @@
+package agents.anac.y2013.TheFawkes;
+
+import negotiator.boaframework.acceptanceconditions.anac2013.AC_TheFawkes;
+import negotiator.boaframework.agent.BOAagent;
+import negotiator.boaframework.offeringstrategy.anac2013.Fawkes_Offering;
+import negotiator.boaframework.omstrategy.TheFawkes_OMS;
+import negotiator.boaframework.opponentmodel.TheFawkes_OM;
+
+public class TheFawkes extends BOAagent {
+
+	@Override
+	public void agentSetup() {
+		opponentModel = new TheFawkes_OM();
+		opponentModel.init(negotiationSession);
+		omStrategy = new TheFawkes_OMS(negotiationSession, opponentModel);
+		offeringStrategy = new Fawkes_Offering();
+		acceptConditions = new AC_TheFawkes();
+		try {
+			offeringStrategy.init(negotiationSession, opponentModel, omStrategy, null);
+			acceptConditions.init(negotiationSession, offeringStrategy, opponentModel, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "TheFawkes";
+	}
+}
