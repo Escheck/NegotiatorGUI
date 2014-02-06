1745a4ede7f35b95dd51743a472a26f80e7d65e4
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2014-02-06 15:33:56 +0000

    #867 agents that can not deal with nonlinear spaces have been marked.

--- NegotiatorGUI/src/agents/TimeDependentAgentConceder.java
+++ NegotiatorGUI/src/agents/TimeDependentAgentConceder.java
@@ -1,16 +1,20 @@
 package agents;
 
-public class TimeDependentAgentConceder extends TimeDependentAgent
-{
+import negotiator.SupportedNegotiationSetting;
+
+public class TimeDependentAgentConceder extends TimeDependentAgent {
 	@Override
-	public double getE()
-	{
+	public double getE() {
 		return 2;
 	}
-	
+
 	@Override
-	public String getName()
-	{
+	public String getName() {
 		return "Conceder";
+	}
+
+	@Override
+	public SupportedNegotiationSetting getSupportedNegotiationSetting() {
+		return SupportedNegotiationSetting.getLinearUtilitySpaceInstance();
 	}
 }
