bad52027d1f0a60627dc421eb8cec2ae76f480b0
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2014-02-12 15:12:35 +0000

    #881 default settings in tournament initialization. Disabled auction and multiPhaseAuction in XML file

--- NegotiatorGUI/src/negotiator/gui/tournamentvars/ProtocolVarUI.java
+++ NegotiatorGUI/src/negotiator/gui/tournamentvars/ProtocolVarUI.java
@@ -1,54 +1,62 @@
 package negotiator.gui.tournamentvars;
 
-
 import java.awt.Frame;
+import java.awt.Panel;
 import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JRadioButton;
-import java.awt.Panel;
-import negotiator.repository.*;
-import negotiator.gui.DefaultOKCancelDialog;
+
 import negotiator.exceptions.Warning;
+import negotiator.gui.DefaultOKCancelDialog;
+import negotiator.repository.ProtocolRepItem;
+import negotiator.repository.RepItem;
+import negotiator.repository.Repository;
+
 /**
  * Open a UI and negotiate with user about which agents to use in tournament.
+ * 
  * @author wouter
- *
+ * 
  */
 public class ProtocolVarUI extends DefaultOKCancelDialog {
 
 	private static final long serialVersionUID = -6106919299675060907L;
-	ArrayList<ProtocolRadioButton> radioButtons; // copy of what's in the panel, for easy check-out. 
+	ArrayList<ProtocolRadioButton> radioButtons; // copy of what's in the panel,
+													// for easy check-out.
 
 	public ProtocolVarUI(Frame owner) {
 		super(owner, "Protocol Variable Selector");
-		
+
 	}
-	
+
 	public Panel getPanel() {
-		radioButtons=new ArrayList<ProtocolRadioButton>();
-		Panel protocolList=new Panel();
-		protocolList.setLayout(new BoxLayout(protocolList,BoxLayout.Y_AXIS));
+		radioButtons = new ArrayList<ProtocolRadioButton>();
+		Panel protocolList = new Panel();
+		protocolList.setLayout(new BoxLayout(protocolList, BoxLayout.Y_AXIS));
 		ButtonGroup group = new ButtonGroup();
 
-		Repository protocolRep=Repository.getProtocolRepository();
-		for (RepItem agt: protocolRep.getItems()) 
-		{
+		Repository protocolRep = Repository.getProtocolRepository();
+		for (RepItem agt : protocolRep.getItems()) {
 			if (!(agt instanceof ProtocolRepItem))
-				new Warning("there is a non-AgentRepItem in agent repository:"+agt);
-			ProtocolRadioButton cbox=new ProtocolRadioButton((ProtocolRepItem)agt);
+				new Warning("there is a non-AgentRepItem in agent repository:"
+						+ agt);
+			ProtocolRadioButton cbox = new ProtocolRadioButton(
+					(ProtocolRepItem) agt);
 			radioButtons.add(cbox);
 			protocolList.add(cbox);
+			cbox.setSelected(true);
 			group.add(cbox);
 		}
 		return protocolList;
 	}
-	
-	public Object ok() { 		
-		ArrayList<ProtocolRepItem> result=new ArrayList<ProtocolRepItem>();
-		for (ProtocolRadioButton cbox: radioButtons) {
-			if (cbox.isSelected()) result.add(cbox.protocolRepItem);
+
+	public Object ok() {
+		ArrayList<ProtocolRepItem> result = new ArrayList<ProtocolRepItem>();
+		for (ProtocolRadioButton cbox : radioButtons) {
+			if (cbox.isSelected())
+				result.add(cbox.protocolRepItem);
 		}
 		return result;
 	}
@@ -56,8 +64,9 @@ public class ProtocolVarUI extends DefaultOKCancelDialog {
 
 class ProtocolRadioButton extends JRadioButton {
 	public ProtocolRepItem protocolRepItem;
-	public ProtocolRadioButton(ProtocolRepItem protocolRepItem) { 
-		super(""+protocolRepItem.getName()); 
+
+	public ProtocolRadioButton(ProtocolRepItem protocolRepItem) {
+		super("" + protocolRepItem.getName());
 		this.protocolRepItem = protocolRepItem;
 	}
 }
--- NegotiatorGUI/src/negotiator/tournament/VariablesAndValues/TotalSessionNumberVariable.java
+++ NegotiatorGUI/src/negotiator/tournament/VariablesAndValues/TotalSessionNumberVariable.java
@@ -1,11 +1,15 @@
 package negotiator.tournament.VariablesAndValues;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class TotalSessionNumberVariable extends TournamentVariable {
 
 	private static final long serialVersionUID = -2284816489963319256L;
 
 	@Override
-	public void addValue(TournamentValue value) throws Exception { }
+	public void addValue(TournamentValue value) throws Exception {
+		throw new NotImplementedException();
+	}
 
 	@Override
 	public String varToString() {
