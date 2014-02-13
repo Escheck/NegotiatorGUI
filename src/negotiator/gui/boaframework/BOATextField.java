3fb351287ee580b56454a872fa057611d3c84219
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2014-02-13 07:48:30 +0000

    #883 text field in BOAagentsFrame now greyed out

--- src/negotiator/gui/boaframework/BOATextField.java
+++ src/negotiator/gui/boaframework/BOATextField.java
@@ -11,17 +11,18 @@ public class BOATextField extends JTextField {
 
 	private static final long serialVersionUID = -2878208768469052209L;
 	private ArrayList<BOAparameter> boaParameters;
-	
+
 	public BOATextField(final Frame frame) {
 		super();
 		this.setEditable(false);
+		this.setEnabled(false);
 	}
-	
+
 	public void setText(ArrayList<BOAparameter> boaParameters) {
 		this.boaParameters = boaParameters;
 		setText(boaParameters.toString());
 	}
-	
+
 	public ArrayList<BOAparameter> getBOAparameters() {
 		return boaParameters;
 	}
