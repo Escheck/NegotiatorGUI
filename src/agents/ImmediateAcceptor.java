3c338b23d2ffcb0ad6d3aa0dd84294abee089cb6
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2014-03-24 09:59:24 +0000

    #909 Immediate Acceptor is not compatible with non-linear spaces. Fixed a bit of javadoc.

--- src/agents/ImmediateAcceptor.java
+++ src/agents/ImmediateAcceptor.java
@@ -1,31 +1,33 @@
 package agents;
 
 import negotiator.Bid;
+import negotiator.SupportedNegotiationSetting;
 
 /**
- * This agent does not concede, but will accept anything equal to or above the reservation value.
- * For undiscounted domain only.
+ * This agent does not concede, but will accept anything equal to or above the
+ * reservation value. For undiscounted domain only.
  */
-public class ImmediateAcceptor extends TimeDependentAgent
-{
+public class ImmediateAcceptor extends TimeDependentAgent {
 	@Override
-	public double getE()
-	{
+	public double getE() {
 		return 0;
 	}
-	
+
 	@Override
-	public String getName()
-	{
+	public String getName() {
 		return "Immediate Acceptor";
 	}
-	
+
 	@Override
-	public boolean isAcceptable(Bid plannedBid)
-	{
+	public boolean isAcceptable(Bid plannedBid) {
 		Bid opponentLastBid = getOpponentLastBid();
-		if(getUtility(opponentLastBid) >= utilitySpace.getReservationValue())
+		if (getUtility(opponentLastBid) >= utilitySpace.getReservationValue())
 			return true;
 		return false;
+	}
+
+	@Override
+	public SupportedNegotiationSetting getSupportedNegotiationSetting() {
+		return SupportedNegotiationSetting.getLinearUtilitySpaceInstance();
 	}
 }
