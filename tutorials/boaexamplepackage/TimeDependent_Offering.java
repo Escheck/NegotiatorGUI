91b9256b00bd8318c1c5a42bd3b3b329ff677e3d
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2014-03-25 10:53:58 +0000

    #876 fixed boaexamplepackage. This fixes problems with the example not running properly if user did not set the parameters (they are proposed automatically now)

--- tutorials/boaexamplepackage/TimeDependent_Offering.java
+++ tutorials/boaexamplepackage/TimeDependent_Offering.java
@@ -1,8 +1,12 @@
 package boaexamplepackage;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import negotiator.bidding.BidDetails;
+import negotiator.boaframework.BOAparameter;
 import negotiator.boaframework.NegotiationSession;
 import negotiator.boaframework.OMStrategy;
 import negotiator.boaframework.OfferingStrategy;
@@ -11,16 +15,16 @@ import negotiator.boaframework.SortedOutcomeSpace;
 import negotiator.boaframework.opponentmodel.NoModel;
 
 /**
- * This is an abstract class used to implement a TimeDependentAgent Strategy adapted from [1]
- * 	[1]	S. Shaheen Fatima  Michael Wooldridge  Nicholas R. Jennings
- * 		Optimal Negotiation Strategies for Agents with Incomplete Information
- * 		http://eprints.ecs.soton.ac.uk/6151/1/atal01.pdf
+ * This is an abstract class used to implement a TimeDependentAgent Strategy
+ * adapted from [1] [1] S. Shaheen Fatima Michael Wooldridge Nicholas R.
+ * Jennings Optimal Negotiation Strategies for Agents with Incomplete
+ * Information http://eprints.ecs.soton.ac.uk/6151/1/atal01.pdf
  * 
  * The default strategy was extended to enable the usage of opponent models.
  * 
- * Note that this agent is not fully equivalent to the theoretical model, loading the domain
- * may take some time, which may lead to the agent skipping the first bid. A better implementation
- * is GeniusTimeDependent_Offering. 
+ * Note that this agent is not fully equivalent to the theoretical model,
+ * loading the domain may take some time, which may lead to the agent skipping
+ * the first bid. A better implementation is GeniusTimeDependent_Offering.
  * 
  * @author Alex Dirkzwager, Mark Hendrikx
  */
@@ -36,59 +40,68 @@ public class TimeDependent_Offering extends OfferingStrategy {
 	private double e;
 	/** Outcome space */
 	SortedOutcomeSpace outcomespace;
-	
+
 	/**
-	 * Empty constructor used for reflexion. Note this constructor assumes that init
-	 * is called next.
+	 * Empty constructor used for reflexion. Note this constructor assumes that
+	 * init is called next.
 	 */
-	public TimeDependent_Offering(){}
-	
-	public TimeDependent_Offering(NegotiationSession negoSession, OpponentModel model, OMStrategy oms, double e, double k, double max, double min){
+	public TimeDependent_Offering() {
+	}
+
+	public TimeDependent_Offering(NegotiationSession negoSession,
+			OpponentModel model, OMStrategy oms, double e, double k,
+			double max, double min) {
 		this.e = e;
 		this.k = k;
 		this.Pmax = max;
 		this.Pmin = min;
 		this.negotiationSession = negoSession;
-		outcomespace = new SortedOutcomeSpace(negotiationSession.getUtilitySpace());
+		outcomespace = new SortedOutcomeSpace(
+				negotiationSession.getUtilitySpace());
 		negotiationSession.setOutcomeSpace(outcomespace);
 		this.opponentModel = model;
-		this.omStrategy = oms;	
+		this.omStrategy = oms;
 	}
-	
+
 	/**
-	 * Method which initializes the agent by setting all parameters.
-	 * The parameter "e" is the only parameter which is required.
+	 * Method which initializes the agent by setting all parameters. The
+	 * parameter "e" is the only parameter which is required.
 	 */
-	public void init(NegotiationSession negoSession, OpponentModel model, OMStrategy oms, HashMap<String, Double> parameters) throws Exception {
+	public void init(NegotiationSession negoSession, OpponentModel model,
+			OMStrategy oms, HashMap<String, Double> parameters)
+			throws Exception {
 		if (parameters.get("e") != null) {
 			this.negotiationSession = negoSession;
-			
-			outcomespace = new SortedOutcomeSpace(negotiationSession.getUtilitySpace());
+
+			outcomespace = new SortedOutcomeSpace(
+					negotiationSession.getUtilitySpace());
 			negotiationSession.setOutcomeSpace(outcomespace);
-			
+
 			this.e = parameters.get("e");
-			
+
 			if (parameters.get("k") != null)
 				this.k = parameters.get("k");
 			else
 				this.k = 0;
-			
+
 			if (parameters.get("min") != null)
 				this.Pmin = parameters.get("min");
 			else
-				this.Pmin = negoSession.getMinBidinDomain().getMyUndiscountedUtil();
-		
+				this.Pmin = negoSession.getMinBidinDomain()
+						.getMyUndiscountedUtil();
+
 			if (parameters.get("max") != null) {
-				Pmax= parameters.get("max");
+				Pmax = parameters.get("max");
 			} else {
 				BidDetails maxBid = negoSession.getMaxBidinDomain();
 				Pmax = maxBid.getMyUndiscountedUtil();
 			}
-			
+
 			this.opponentModel = model;
 			this.omStrategy = oms;
 		} else {
-			throw new Exception("Constant \"e\" for the concession speed was not set.");
+			throw new Exception(
+					"Constant \"e\" for the concession speed was not set.");
 		}
 	}
 
@@ -98,52 +111,55 @@ public class TimeDependent_Offering extends OfferingStrategy {
 	}
 
 	/**
-	 * Simple offering strategy which retrieves the target utility
-	 * and looks for the nearest bid if no opponent model is specified.
-	 * If an opponent model is specified, then the agent return a bid according
-	 * to the opponent model strategy.
+	 * Simple offering strategy which retrieves the target utility and looks for
+	 * the nearest bid if no opponent model is specified. If an opponent model
+	 * is specified, then the agent return a bid according to the opponent model
+	 * strategy.
 	 */
 	@Override
 	public BidDetails determineNextBid() {
 		double time = negotiationSession.getTime();
 		double utilityGoal;
 		utilityGoal = p(time);
-		
-//		System.out.println("[e=" + e + ", Pmin = " + BilateralAgent.round2(Pmin) + "] t = " + BilateralAgent.round2(time) + ". Aiming for " + utilityGoal);
-		
+
+		// System.out.println("[e=" + e + ", Pmin = " +
+		// BilateralAgent.round2(Pmin) + "] t = " + BilateralAgent.round2(time)
+		// + ". Aiming for " + utilityGoal);
+
 		// if there is no opponent model available
 		if (opponentModel instanceof NoModel) {
-			nextBid = negotiationSession.getOutcomeSpace().getBidNearUtility(utilityGoal);
+			nextBid = negotiationSession.getOutcomeSpace().getBidNearUtility(
+					utilityGoal);
 		} else {
 			nextBid = omStrategy.getBid(outcomespace, utilityGoal);
 		}
 		return nextBid;
 	}
-	
+
 	/**
 	 * From [1]:
 	 * 
-	 * A wide range of time dependent functions can be defined by varying the way in
-	 * which f(t) is computed. However, functions must ensure that 0 <= f(t) <= 1,
-	 * f(0) = k, and f(1) = 1.
+	 * A wide range of time dependent functions can be defined by varying the
+	 * way in which f(t) is computed. However, functions must ensure that 0 <=
+	 * f(t) <= 1, f(0) = k, and f(1) = 1.
 	 * 
-	 * That is, the offer will always be between the value range, 
-	 * at the beginning it will give the initial constant and when the deadline is reached, it
-	 * will offer the reservation value.
+	 * That is, the offer will always be between the value range, at the
+	 * beginning it will give the initial constant and when the deadline is
+	 * reached, it will offer the reservation value.
 	 * 
 	 * For e = 0 (special case), it will behave as a Hardliner.
 	 */
-	public double f(double t)
-	{
+	public double f(double t) {
 		if (e == 0)
 			return k;
-		double ft = k + (1 - k) * Math.pow(t, 1.0/e);
+		double ft = k + (1 - k) * Math.pow(t, 1.0 / e);
 		return ft;
 	}
 
 	/**
-	 * Makes sure the target utility with in the acceptable range according to the domain
-	 * Goes from Pmax to Pmin!
+	 * Makes sure the target utility with in the acceptable range according to
+	 * the domain Goes from Pmax to Pmin!
+	 * 
 	 * @param t
 	 * @return double
 	 */
@@ -153,5 +169,16 @@ public class TimeDependent_Offering extends OfferingStrategy {
 
 	public NegotiationSession getNegotiationSession() {
 		return negotiationSession;
+	}
+
+	@Override
+	public Set<BOAparameter> getParameters() {
+		Set<BOAparameter> set = new HashSet<BOAparameter>();
+		set.add(new BOAparameter("e", new BigDecimal(1.0), "Concession rate"));
+		set.add(new BOAparameter("k", new BigDecimal(0.0), "Offset"));
+		set.add(new BOAparameter("min", new BigDecimal(0.0), "Minimum utility"));
+		set.add(new BOAparameter("max", new BigDecimal(0.99), "Maximum utility"));
+
+		return set;
 	}
 }
