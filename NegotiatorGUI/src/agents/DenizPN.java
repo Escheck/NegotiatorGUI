9045cee3f6df597c13df7623e78c876ef6fca94e
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2014-09-04 13:41:32 +0000

    #944 added Deniz agent. SimlpleTitForTat shares code now using SimpleFakeSpace.

---
+++ NegotiatorGUI/src/agents/DenizPN.java
@@ -0,0 +1,394 @@
+package agents;
+
+import java.util.Set;
+
+import negotiator.Agent;
+import negotiator.Bid;
+import negotiator.DiscreteTimeline;
+import negotiator.PocketNegotiatorAgent;
+import negotiator.Timeline;
+import negotiator.actions.Accept;
+import negotiator.actions.Action;
+import negotiator.actions.Offer;
+import negotiator.analysis.BidPoint;
+import negotiator.utility.UtilitySpace;
+
+/**
+ * Agent proposed by Reynan #944. Notice, this agent only works with a
+ * {@link DiscreteTimeline} just like the {@link OptimalBidder}.
+ * 
+ * @author W.Pasman 2sep2014
+ * 
+ */
+public class DenizPN extends Agent implements PocketNegotiatorAgent {
+
+	/**
+	 * We have a hard coded reservation value now. To be fixed later to use
+	 * utilitySpace.getReservationValue()
+	 */
+	private final double RESERVATION_VALUE = 0.5;
+
+	/**
+	 * The move that our agent determines to make.
+	 */
+	private enum MyMoves {
+		/** best of concession or accept */
+		CONCEDE,
+		/** best of concession, accept, or opponent bid projected onto pareto */
+		CONCEDE_OR_PARETO,
+		/** repeat our last bid */
+		SAME,
+		/** a bid with same utils as our last bid, but different */
+		SILENT
+	};
+
+	HistorySpace historySpace = new HistorySpace();
+
+	/************* implements PocketNegotiatorAgent ***************/
+	@Override
+	public void initPN(UtilitySpace mySide, UtilitySpace otherSide, Timeline tl) {
+		updateProfiles(mySide, otherSide);
+		timeline = tl;
+	}
+
+	@Override
+	public void handleAction(Action act) {
+		ReceiveMessage(act);
+
+	}
+
+	@Override
+	public Action getAction() {
+		return chooseAction();
+	}
+
+	@Override
+	public void updateProfiles(UtilitySpace myUtilities,
+			UtilitySpace opponentUtilities) {
+		utilitySpace = myUtilities; // keep Agent happy... For Genius
+		historySpace.setUtilities(myUtilities, opponentUtilities);
+	}
+
+	/**************** extends Agent *******************/
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public String getName() {
+		return "Deniz agent PN";
+	}
+
+	@Override
+	public void ReceiveMessage(Action opponentAction) {
+		try {
+			if (opponentAction instanceof Offer) {
+				receiveMessage1(((Offer) opponentAction).getBid());
+			}
+		} catch (Exception e) {
+			// this should be fatal. But
+			// we can't do much, ReceiveMessage does not allow throw.
+			e.printStackTrace();
+		}
+	}
+
+	private void receiveMessage1(Bid bid) throws Exception {
+		historySpace.getOpponentBids().add(bid);
+
+		if (historySpace.getOutcomeSpace() == null) {
+			// if this is null, we are running in Genius.
+			historySpace.setUtilities(utilitySpace,
+					new SimpleFakeOpponentUtilitySpace(utilitySpace, bid));
+		}
+	}
+
+	@Override
+	public Action chooseAction() {
+		/**
+		 * default action. Also returned if exception occurs. Notice, that may
+		 * be a protocol error but what else can we do?
+		 */
+
+		Action action = new Accept();
+
+		try {
+			Bid bid = chooseAction1();
+			Bid effectiveBid;
+			if (bid != null) {
+				action = new Offer(bid);
+				effectiveBid = bid;
+			} else {
+				effectiveBid = historySpace.getOpponentBids().last();
+			}
+			historySpace.getMyBids().add(effectiveBid);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return action;
+	}
+
+	/**
+	 * get next bid. Returns null for accept. returns max util bid if
+	 * historySpace null (which can happen if running from Genius and opponent
+	 * did not yet place bid) or if we did not yet place initial bid.
+	 * 
+	 * @return chosen bid, or null to accept last opponent bid.
+	 * @throws Exception
+	 *             if fatal problem occurs somewhere in our code.
+	 */
+	private Bid chooseAction1() throws Exception {
+		BidPoint myNextBid = null;
+
+		if (historySpace.getOpponentBids().isEmpty()
+				|| historySpace.getMyBids().isEmpty()) {
+			// First round. place our best bid
+			return utilitySpace.getMaxUtilityBid();
+		}
+
+		// below this point, both sides did initial bid
+		switch (determineMyMove()) {
+		case CONCEDE:
+			myNextBid = getConcessionBid();
+			break;
+		case CONCEDE_OR_PARETO:
+			myNextBid = getConcedeOrPareto();
+			break;
+		case SAME:
+			myNextBid = bidPoint(historySpace.getMyBids().last());
+			break;
+		case SILENT:
+			myNextBid = bidPoint(historySpace.getSilentBid());
+			break;
+		}
+
+		if (acceptOpponentBid(myNextBid)) {
+			return null;
+		}
+
+		return myNextBid.getBid();
+	}
+
+	/**
+	 * Checks if we should accept opponent's last bid.
+	 * 
+	 * @param mynextbid
+	 *            the next bid that WE will place, if we do not accept the
+	 *            opponents bid.
+	 * 
+	 * @return true if bid=null, if the opponent's last bid is better than
+	 *         mynextbid, or if the opponent's last bid has utility > current
+	 *         target utility (see {@link #getTargetUtility()}.
+	 * @throws Exception
+	 */
+	private boolean acceptOpponentBid(BidPoint mynextbid) throws Exception {
+		if (mynextbid == null) {
+			return true;
+		}
+		Bid lastopponentbid = historySpace.getOpponentBids().last();
+		double lastutil = historySpace.getOutcomeSpace().getMyUtilitySpace()
+				.getUtility(lastopponentbid);
+		if (mynextbid.getUtilityA() < lastutil) {
+			return true;
+		}
+		if (lastutil > getTargetUtility()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Determine a move according to the table. The move that we do depends on
+	 * the opponent's moves. Actually we are using a reversed version of the
+	 * table (columns are the opponent moves: his current, one back and two
+	 * back) and after the arrow it's the move that we decide to do: {self means
+	 * selfish or silent; nonself means any other}
+	 * <ol>
+	 * <li>self , self , self -> SAME
+	 * <li>self, self, nonself -> CONCEDE_OR_ACCEPT
+	 * <li>self, nonself -> SILENT
+	 * <li>nice/concession/fortunate -> CONCEDE_OR_ACCEPT
+	 * <li>unfortunate -> PARETO_OR_CONCEDE_OR_ACCEPT
+	 * </ol>
+	 * Assumes that we did one offer and the other side did already 2 offers at
+	 * least.
+	 * 
+	 * @return
+	 */
+	private MyMoves determineMyMove() {
+		// check opponent previous moves. See the table above.
+		MoveType move0 = historySpace.getMoveType(0);
+		if (isSelfish(move0)) {
+			if (isSelfish(historySpace.getMoveType(1))) {
+				if (isSelfish(historySpace.getMoveType(2))) {
+					return MyMoves.SAME;
+				}
+				// last 2 moves selfish, but before that not.
+				return MyMoves.CONCEDE;
+			}
+			// current move selfish, previous move not
+			return MyMoves.SILENT;
+		}
+		// current move not selfish
+		if (move0 == MoveType.UNFORTUNATE) {
+			return MyMoves.CONCEDE_OR_PARETO;
+		}
+		return MyMoves.CONCEDE;
+	}
+
+	private boolean isSelfish(MoveType move) {
+		return move == MoveType.SELFISH || move == MoveType.SILENT;
+	}
+
+	/**
+	 * check the concession bid against the on-pareto-projected-other-bid and
+	 * return the one with the highest our-utility
+	 * 
+	 * @return concession, accept or paretoprojected opponent bid.
+	 */
+	private BidPoint getConcedeOrPareto() {
+		return bestBid(projectOtherBidToPareto(), getConcessionBid());
+	}
+
+	/**
+	 * create a new bid by mapping the other's last bid to the pareto frontier.
+	 * *
+	 * 
+	 * @return opponent's last bid mapped to pareto, or concession bid.
+	 */
+	private BidPoint projectOtherBidToPareto() {
+		SimpleBidSpace space = historySpace.getOutcomeSpace();
+		double otherUtil;
+		try {
+			otherUtil = space.getOpponentUtilitySpace().getUtility(
+					historySpace.getOpponentBids().last());
+		} catch (Exception e) {
+			throw new IllegalStateException("getUtility failed", e);
+		}
+
+		return space.getPareto().getBidNearOpponentUtility(otherUtil);
+	}
+
+	/**
+	 * Tries to concede. The starting point is a bid from the optimalbidding
+	 * strategy. But instead of using that right away, we search around to find
+	 * some other bidpoints close to that optimal bidpoint. We pick the bid that
+	 * in the neighbourhood that has a minimal hamming distance with the
+	 * opponent's last bid.
+	 * 
+	 * <h1>Assumes</h1> that opponent did place at least one bid.
+	 * 
+	 * @return concession bid according to {@link OptimalBidder}.
+	 * @throws Exception
+	 */
+	private BidPoint getConcessionBid() {
+		BidPoint optimalbid = getOptimalBid();
+		Bid lastopponentbid = historySpace.getOpponentBids().last();
+		Set<BidPoint> nearoptimalbids = historySpace.getOutcomeSpace()
+				.getNearUtility(optimalbid, 0.05);
+		return getSmallestHamming(nearoptimalbids, lastopponentbid);
+	}
+
+	/**
+	 * Find in a set of BidPoints the one that has highest similarity (see
+	 * {@link Bid#getSimilarity(Bid)}) to given target bid.
+	 * 
+	 * @param bids
+	 *            set of bids to pick from
+	 * @param targetbid
+	 *            the
+	 * @return a bid that has highest similarity with given target bid. May
+	 *         return null if the given bids list is empty.
+	 */
+	private BidPoint getSmallestHamming(Set<BidPoint> bids, Bid lastopponentbid) {
+		BidPoint bestBid = null;
+		double bestsimilarity = -1; // lower than worst similarity
+		for (BidPoint bidpoint : bids) {
+			double similarity = bidpoint.getBid()
+					.getSimilarity(lastopponentbid);
+			if (similarity > bestsimilarity) {
+				bestBid = bidpoint;
+				bestsimilarity = similarity;
+			}
+		}
+		return bestBid;
+	}
+
+	/**
+	 * Get the bid that has biggest myUtility.
+	 * 
+	 * @param point1
+	 *            first bid
+	 * @param point2
+	 *            second bid
+	 * @return best bid of the two points provided.
+	 */
+	private BidPoint bestBid(BidPoint point1, BidPoint point2) {
+		if (isBetter(point1, point2)) {
+			return point1;
+		}
+		return point2;
+	}
+
+	/**
+	 * Would be nice to have this in BidPoint.
+	 * 
+	 * @param point1
+	 * @param point2
+	 * @return true if point1 is better for us than point2.
+	 */
+	private boolean isBetter(BidPoint point1, BidPoint point2) {
+		return point1.getUtilityA() >= point2.getUtilityA();
+	}
+
+	/**
+	 * Create a bid point for a given bid
+	 * 
+	 * @param bid
+	 * @return {@link BidPoint}.
+	 */
+	private BidPoint bidPoint(Bid bid) {
+		return historySpace.getOutcomeSpace().bidPoint(bid);
+	}
+
+	/****************** OptimalBidder code *************/
+
+	/**
+	 * The optimal bid is a bid on the pareto with a target utility
+	 * {@link #bid(remain)} where remain is the remaining turns for me.
+	 *
+	 */
+	private BidPoint getOptimalBid() {
+		return historySpace.getOutcomeSpace().getPareto()
+				.getBidNearMyUtility(getTargetUtility());
+	}
+
+	/**
+	 * Target utility that we want right now.
+	 * 
+	 * @return current targetutility.
+	 */
+	private double getTargetUtility() {
+		int roundsleft = ((DiscreteTimeline) timeline).getOwnRoundsLeft();
+		Double targetUtil = bid(roundsleft + 1);
+		return targetUtil;
+	}
+
+	/**
+	 * computation of the bid for round j as in prop 4.3. Basically this
+	 * increases with each round, starting at 0.5 + reservation value.
+	 * 
+	 * @param round
+	 *            j starting at round 1.
+	 * @return bid value is a target utility.
+	 **/
+
+	private double bid(int j) {
+		if (j == 1)
+			return 0.5 + 0.5 * RESERVATION_VALUE;
+		else
+			return 0.5 + 0.5 * Math.pow(bid(j - 1), 2);
+	}
+
+}
---
+++ NegotiatorGUI/src/agents/SimpleBidSpace.java
@@ -0,0 +1,151 @@
+package agents;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import negotiator.Bid;
+import negotiator.BidIterator;
+import negotiator.analysis.BidPoint;
+import negotiator.utility.UtilitySpace;
+import agents.anac.y2010.Southampton.analysis.BidSpace;
+
+/**
+ * A simple version of {@link BidSpace} that does not mess with files, and
+ * allows changing and retrieving the utility spaces that are contained here.
+ * Utility spaces should not be changed directly, because the ParetoFrontier can
+ * not see such changes. {@link #changeSpaces} should be called when the space
+ * changes.
+ * 
+ * @author W.Pasman 2sep14
+ *
+ */
+public class SimpleBidSpace {
+
+	private UtilitySpace mySpace, otherSpace;
+
+	// lazy init to avoid duplicate computations.
+	private ParetoFrontierPlus pareto = null;
+
+	/**
+	 * 
+	 * @param my
+	 *            {@link UtilitySpace} of mySide
+	 * @param other
+	 *            {@link UtilitySpace} of otherSide.
+	 */
+	public SimpleBidSpace(UtilitySpace my, UtilitySpace other) {
+		changeSpaces(my, other);
+	}
+
+	/**
+	 * select a new utility space. Might work incrementally but currently does
+	 * not do that.
+	 * 
+	 * @param my
+	 *            {@link UtilitySpace} of mySide
+	 * @param other
+	 *            {@link UtilitySpace} of otherSide
+	 */
+	public void changeSpaces(UtilitySpace my, UtilitySpace other) {
+		if (my == null || other == null) {
+			throw new NullPointerException("my or other UtilitySpace is null");
+		}
+		this.mySpace = my;
+		this.otherSpace = other;
+		pareto = null;
+
+	}
+
+	/**
+	 * Utility spaces should not be changed directly, because the ParetoFrontier
+	 * can not see such changes.
+	 * 
+	 * @return own's {@link UtilitySpace}.
+	 */
+	public UtilitySpace getMyUtilitySpace() {
+		return mySpace;
+	}
+
+	/**
+	 * Utility spaces should not be changed directly, because the ParetoFrontier
+	 * can not see such changes.
+	 * 
+	 * @return the opponent's {@link UtilitySpace}.
+	 */
+	public UtilitySpace getOpponentUtilitySpace() {
+		return otherSpace;
+	}
+
+	public ParetoFrontierPlus getPareto() {
+		if (pareto == null) {
+			pareto = new ParetoFrontierPlus(mySpace, otherSpace);
+		}
+		return pareto;
+	}
+
+	/**
+	 * Find a bit that has utilities similar to given bid but is not equal to
+	 * it. SortedOutcomeSpace does not help here because it sorts only in 1
+	 * dimension and can not return a 2-D closest point.
+	 * 
+	 * 
+	 * FIXME use maximum distance 0.05 use getNearUtility and take random one
+	 * from that set different from previous bid.
+	 * 
+	 * @param last
+	 * @return bid similar to given bid but not equal, unless there is no nearby
+	 *         other point. This bid may not lie on the pareto.
+	 */
+	public Bid getSilentBid(Bid bid) {
+		BidPoint bidpt = bidPoint(bid);
+		Set<BidPoint> nearby = getNearUtility(bidpt, 0.05);
+		nearby.remove(bidpt);
+		if (nearby.isEmpty()) {
+			return bid;
+		}
+		// pick random
+		ArrayList<BidPoint> nearbylist = new ArrayList<BidPoint>(nearby);
+		return nearbylist.get((int) Math.random() * nearbylist.size()).getBid();
+	}
+
+	/**
+	 * Convert bid into bid point
+	 * 
+	 * @param bid
+	 * @return {@link BidPoint} for given bid.
+	 */
+	public BidPoint bidPoint(Bid bid) {
+		try {
+			return new BidPoint(bid, mySpace.getUtility(bid),
+					otherSpace.getUtility(bid));
+		} catch (Exception e) {
+			throw new IllegalStateException("failed to get utility of bid "
+					+ bid, e);
+		}
+	}
+
+	/**
+	 * Get set of bids similar (or equal) to given {@link BidPoint}.
+	 * 
+	 * @param optimalbid
+	 * @param distance
+	 *            maximum euclidean distance to nearby points.
+	 * @return set of {@link BidPoint}s near given point, including the bid
+	 *         itself (assuming distance>0).
+	 */
+	public Set<BidPoint> getNearUtility(BidPoint optimalbid, double distance) {
+		HashSet<BidPoint> similarbids = new BidPointSpace();
+		BidIterator bids = new BidIterator(mySpace.getDomain());
+
+		while (bids.hasNext()) {
+			Bid bid = bids.next();
+			BidPoint bidpoint = bidPoint(bid);
+			if (bidpoint.getDistance(optimalbid) <= distance) {
+				similarbids.add(bidpoint);
+			}
+		}
+
+		return similarbids;
+	}
+}
