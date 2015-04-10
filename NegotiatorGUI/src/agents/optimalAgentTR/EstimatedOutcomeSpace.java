3a7265a46996714c5c5363dd782443d882c805d6
reyhan <reyhan@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-04-10 13:48:54 +0000

    Added another method to generate estimated outcome space.

--- NegotiatorGUI/src/agents/optimalAgentTR/EstimatedOutcomeSpace.java
+++ NegotiatorGUI/src/agents/optimalAgentTR/EstimatedOutcomeSpace.java
@@ -21,6 +21,11 @@ public class EstimatedOutcomeSpace {
 		generateAllBids(myUtilSpace,oppUtilSpace);
 	}
 	
+	public EstimatedOutcomeSpace (UtilitySpace myUtilSpace, UtilitySpace oppUtilSpace, double nonAcceptableUtility, double acceptableUtility) {
+		this.estimatedOutcomeSpace=new ArrayList<BidInfo>();
+		generateAllBids(myUtilSpace,oppUtilSpace,nonAcceptableUtility,acceptableUtility);
+	}
+	
 	
 	public void generateAllBids(UtilitySpace utilSpace) {
 		
@@ -43,6 +48,26 @@ public class EstimatedOutcomeSpace {
 			Bid bid = iter.next();
 			try {
 				BidInfo bidInformation = new BidInfo(bid, utilSpace.getUtility(bid), oppUtilSpace.getUtility(bid));
+				estimatedOutcomeSpace.add(bidInformation);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void generateAllBids(UtilitySpace utilSpace, UtilitySpace oppUtilSpace, double nonAcceptableUtility, double acceptableUtility) {
+		
+		BidIterator iter = new BidIterator(utilSpace.getDomain());
+		while (iter.hasNext()) {
+			Bid bid = iter.next();
+			try {
+				BidInfo bidInformation;
+				if (oppUtilSpace.getUtility(bid)>=acceptableUtility)
+					bidInformation = new BidInfo(bid, utilSpace.getUtility(bid), 1.0);
+				else if (oppUtilSpace.getUtility(bid)<=nonAcceptableUtility)
+					bidInformation = new BidInfo(bid, utilSpace.getUtility(bid), 0.0);
+				else 	
+					bidInformation = new BidInfo(bid, utilSpace.getUtility(bid), (nonAcceptableUtility+((acceptableUtility-oppUtilSpace.getUtility(bid))/(acceptableUtility-nonAcceptableUtility))));
 				estimatedOutcomeSpace.add(bidInformation);
 			} catch (Exception e) {
 				e.printStackTrace();
