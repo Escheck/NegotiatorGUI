8cac34f60480a3fb27d80006480ad12a3f299870
reyhan <reyhan@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-04-14 12:28:16 +0000

    Utility based agent has been added.

---
+++ src/agents/UtilityBasedAcceptor.java
@@ -0,0 +1,37 @@
+package agents;
+
+import negotiator.Bid;
+import negotiator.SupportedNegotiationSetting;
+
+/**
+ * This agent does not concede, but will accept anything equal to or above the
+ * reservation value. For undiscounted domain only.
+ */
+public class UtilityBasedAcceptor extends TimeDependentAgent {
+	@Override
+	public double getE() {
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		return "Utility Based Acceptor";
+	}
+
+	@Override
+	public boolean isAcceptable(Bid plannedBid) {
+		Bid opponentLastBid = getOpponentLastBid();
+		if (getUtility(opponentLastBid) >= utilitySpace.getReservationValue()){
+		
+			if (Math.random()>= getUtility(opponentLastBid))
+				return true;
+			else return false;
+		}
+		return false;
+	}
+
+	@Override
+	public SupportedNegotiationSetting getSupportedNegotiationSetting() {
+		return SupportedNegotiationSetting.getLinearUtilitySpaceInstance();
+	}
+}
