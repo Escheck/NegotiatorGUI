16b96c7f34da433cd6eed15953625f07b48c33f9
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-06-17 07:55:11 +0000

    #1058 added more tests. Few are failing as already expected in the ticket.

--- NegotiatorGUI/test/negotiator/protocol/AlternatingOfferConsensusProtocolTest.java
+++ NegotiatorGUI/test/negotiator/protocol/AlternatingOfferConsensusProtocolTest.java
@@ -1,6 +1,7 @@
 package negotiator.protocol;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import negotiator.Deadline;
 import negotiator.actions.Accept;
 import negotiator.actions.OfferForVoting;
 import negotiator.actions.Reject;
@@ -29,6 +31,10 @@ public class AlternatingOfferConsensusProtocolTest {
 
 	Turn turn = mock(Turn.class);
 
+	private static int VOTING_ROUND = 0;
+	private static int OFFER_ROUND = 1;
+	private static int VOTING_ROUND_2 = 2;
+
 	// Round round = mock(Round.class);
 
 	Session session = mock(Session.class);
@@ -43,7 +49,7 @@ public class AlternatingOfferConsensusProtocolTest {
 
 	@Test
 	public void testGetRoundStructureVotingRound() {
-		when(session.getRoundNumber()).thenReturn(0); // an OFFER round
+		when(session.getRoundNumber()).thenReturn(VOTING_ROUND);
 
 		List<NegotiationParty> parties = new ArrayList<NegotiationParty>();
 		NegotiationParty party1 = mock(NegotiationParty.class);
@@ -75,7 +81,7 @@ public class AlternatingOfferConsensusProtocolTest {
 
 	@Test
 	public void testGetRoundStructureOfferRound() {
-		when(session.getRoundNumber()).thenReturn(1); // an VOTING round.
+		when(session.getRoundNumber()).thenReturn(OFFER_ROUND);
 
 		// the specs has poor type, that's why we have it here too.
 		Collection<Class> acceptOrReject = new ArrayList<Class>(2);
@@ -113,4 +119,60 @@ public class AlternatingOfferConsensusProtocolTest {
 
 	}
 
+	/**
+	 * Call isFinished when in round 0 (initial situation). The round should not
+	 * be finished, nothing happened yet. FAILS because
+	 * get(session.getRoundNumber() - 2) in the isFinished code will give
+	 * indexOutOfBoundsException.
+	 */
+	@Test
+	public void isFinishedTestVoting() {
+		List<NegotiationParty> parties = new ArrayList<NegotiationParty>();
+		when(session.getRoundNumber()).thenReturn(VOTING_ROUND);
+		when(session.getRounds()).thenReturn(new ArrayList<Round>());
+		assertFalse(protocol.isFinished(session, parties));
+	}
+
+	/**
+	 * Call isFinished when in round 1
+	 */
+	@Test
+	public void isFinishedTestNonVoting() {
+		List<NegotiationParty> parties = new ArrayList<NegotiationParty>();
+		when(session.getRoundNumber()).thenReturn(OFFER_ROUND);
+		assertFalse(protocol.isFinished(session, parties));
+	}
+
+	/**
+	 * call isFinished when in round 2 The round should not be finished, nothing
+	 * happened yet.
+	 */
+	@Test
+	public void isFinishedTestVoting2() {
+		List<NegotiationParty> parties = new ArrayList<NegotiationParty>();
+		when(session.getRoundNumber()).thenReturn(VOTING_ROUND_2);
+
+		Round round = mock(Round.class);
+		ArrayList<Turn> turns = new ArrayList<Turn>();
+		when(round.getTurns()).thenReturn(turns);
+		ArrayList<Round> rounds = new ArrayList<Round>();
+		rounds.add(round);
+		when(session.getRounds()).thenReturn(rounds);
+		when(session.getMostRecentRound()).thenReturn(round);
+
+		assertFalse(protocol.isFinished(session, parties));
+	}
+
+	/**
+	 * Check if code works with real round. Currently FAILS with
+	 * indexOutOfBounds.
+	 */
+	@Test
+	public void isFinishedTestVotingEndEndTest() {
+		Session realsession = new Session(new Deadline(5, 5));
+		Round round = new Round();
+		round.addTurn(new Turn(null));
+		List<NegotiationParty> parties = new ArrayList<NegotiationParty>();
+		assertFalse(protocol.isFinished(realsession, parties));
+	}
 }
