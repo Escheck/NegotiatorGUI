816ffed4f8d2b6420e087a96a72f994d40aa5a48
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-06-18 08:34:04 +0000

    #1069 end to end test for StackedAlternatingOffersProtocol is ok

--- test/negotiator/protocol/StackedAlternatingOffersProtocolE2ETest.java
+++ test/negotiator/protocol/StackedAlternatingOffersProtocolE2ETest.java
@@ -26,102 +26,24 @@ import org.junit.Test;
 
 public class StackedAlternatingOffersProtocolE2ETest {
 
-	private static final String BID_PATTERN = "( Offer: Bid [a: .., b: .., c: .., d: .., e: .., f: .., g: .., h: .. , ] ) ";
+	private static final String BID_PATTERN = "Bid [a: .., b: .., c: .., d: .., e: .., f: .., g: .., h: .. , ]";
+	private static final String OFFER_PATTERN = "( Offer: " + BID_PATTERN
+			+ " ) ";
 	private static final String ACCEPT_PATTERN = "( Accept )";
 
-	@Test
-	public void runMultiPartyNego1() throws Exception {
-
-		/** set up a multiparty negotiation */
-		final String[] partyclasses = {
-				"negotiator.parties.BoulwareNegotiationParty",
-				"negotiator.parties.ConcederNegotiationParty",
-				"negotiator.parties.RandomCounterOfferNegotiationParty" };
-		Deadline deadline = new Deadline(0, 60);
-		Session session = new Session(deadline);
-		List<NegotiationParty> parties = new ArrayList<NegotiationParty>();
-
-		// bad to have absolute ref. But there's no better function in
-		// Repository...
-		DomainRepItem domain8 = Repository
-				.getDomainByName("file:etc/templates/Domain8/Domain8.xml");
-		Repository party_rep = Repository.get_party_repository();
-
-		for (int partynr = 0; partynr < 3; partynr++) {
-			ProfileRepItem profileRepItem = domain8.getProfiles().get(partynr);
-			PartyRepItem partyRepItem = party_rep
-					.getPartyOfClass(partyclasses[partynr]);
-
-			NegotiationParty negoparty = TournamentGenerator.createFrom(
-					partyRepItem, profileRepItem, session);
-
-			parties.add(negoparty);
-		}
-		// maybe we can craete the parties directly, using this?
-		// parties.add(new BoulwareNegotiationParty(utilitySpace, deadlines,
-		// timeline, randomSeed));
-
-		MultilateralProtocol protocol = new StackedAlternatingOffersProtocol();
-
-		SessionManager manager = new SessionManager(parties, protocol, session,
-				new ExecutorWithTimeout(
-						1000 * deadline.getTimeOrDefaultTimeout()));
-
-		myListener listener = new myListener();
-
-		manager.addLoggingListener(listener);
-
-		manager.run();
-
-		/*********** and finally check the outcome **************/
-		MultipartyNegotiationSessionEvent lastEvent = listener.getEvents().get(
-				listener.getEvents().size() - 1);
-		assertNotNull(lastEvent.getAgreement());
-
-		// check the logs file. It should be of this form
-		// Starting negotiation session.
-		// SOME NUMBER OF THIS {
-		// Round N
-		// Turn 1: Boulware#.* offerOrAccept
-		// Turn 2: Conceder#.* offerOrAccept
-		// Turn 3: Random#.* offerOrAccept
-		// }
-		// Round .*
-		// Turn 1: .* (Accept)
-		// Turn 2: .* (Accept)
-		// Found an agreement: Bid[a: .., b: .., c: .., d: .., e: .., f: .., g:
-		// .., h: .., ]
-		// Finished negotiation session in .*s
-		// SUMMARY TEXT LINE
-
-		int line = 0;
-
-		assertMatch("Starting .* session.*", listener.getLogs().get(line++));
-
-		int rounds = (listener.getLogs().size() - 7) / 4;
-		for (int n = 0; n < rounds; n++) {
-			System.out.println("check round " + (n + 1));
-			assertMatch("Round " + (n + 1), listener.getLogs().get(line++));
-			assertBidOrAccept(" Turn 1: Boulware\\S* ",
-					listener.getLogs().get(line++));
-			assertBidOrAccept(" Turn 2: Conceder\\S* ",
-					listener.getLogs().get(line++));
-			assertBidOrAccept(" Turn 3: Random\\S* ",
-					listener.getLogs().get(line++));
-		}
-
-	}
-
 	enum BidType {
-		OFFER, ACCEPT
+		OFFER, ACCEPT, PLAIN_BID
 	};
 
 	private BidType getType(String preamble, String text) {
-		if (match(preamble + BID_PATTERN, text)) {
+		if (match(preamble + OFFER_PATTERN, text)) {
 			return BidType.OFFER;
 		}
 		if (match(preamble + ACCEPT_PATTERN, text)) {
 			return BidType.ACCEPT;
+		}
+		if (match(preamble + BID_PATTERN, text)) {
+			return BidType.PLAIN_BID;
 		}
 		throw new IllegalArgumentException("text " + text
 				+ " does not contain offer or accept");
@@ -214,4 +136,110 @@ public class StackedAlternatingOffersProtocolE2ETest {
 		}
 
 	}
+
+	/************************************************************************************************/
+	/*
+	 * the actual tests are below. We can't check creation of log files, as
+	 * these are not created normally.
+	 */
+
+	/**
+	 * End to end test of multiparty tournament. 3 parties boulware, conceder,
+	 * random. Domain8 is used.
+	 */
+	@Test
+	public void runMultiPartyNego1() throws Exception {
+
+		/** set up a multiparty negotiation */
+		final String[] partyclasses = {
+				"negotiator.parties.BoulwareNegotiationParty",
+				"negotiator.parties.ConcederNegotiationParty",
+				"negotiator.parties.RandomCounterOfferNegotiationParty" };
+		Deadline deadline = new Deadline(0, 60);
+		Session session = new Session(deadline);
+		List<NegotiationParty> parties = new ArrayList<NegotiationParty>();
+
+		// bad to have absolute ref. But there's no better function in
+		// Repository...
+		DomainRepItem domain8 = Repository
+				.getDomainByName("file:etc/templates/Domain8/Domain8.xml");
+		Repository party_rep = Repository.get_party_repository();
+
+		for (int partynr = 0; partynr < 3; partynr++) {
+			ProfileRepItem profileRepItem = domain8.getProfiles().get(partynr);
+			PartyRepItem partyRepItem = party_rep
+					.getPartyOfClass(partyclasses[partynr]);
+
+			NegotiationParty negoparty = TournamentGenerator.createFrom(
+					partyRepItem, profileRepItem, session);
+
+			parties.add(negoparty);
+		}
+		// maybe we can craete the parties directly, using this?
+		// parties.add(new BoulwareNegotiationParty(utilitySpace, deadlines,
+		// timeline, randomSeed));
+
+		MultilateralProtocol protocol = new StackedAlternatingOffersProtocol();
+
+		SessionManager manager = new SessionManager(parties, protocol, session,
+				new ExecutorWithTimeout(
+						1000 * deadline.getTimeOrDefaultTimeout()));
+
+		myListener listener = new myListener();
+
+		manager.addLoggingListener(listener);
+
+		manager.run();
+
+		/*********** and finally check the outcome **************/
+		MultipartyNegotiationSessionEvent lastEvent = listener.getEvents().get(
+				listener.getEvents().size() - 1);
+		assertNotNull(lastEvent.getAgreement());
+
+		// check the logs file. It should be of this form
+		// Starting negotiation session.
+		// SOME NUMBER OF THIS {
+		// Round N
+		// Turn 1: Boulware#.* offerOrAccept
+		// Turn 2: Conceder#.* offerOrAccept
+		// Turn 3: Random#.* offerOrAccept
+		// }
+		// Round .*
+		// Turn 1: .* (Accept)
+		// Turn 2: .* (Accept)
+		// Found an agreement: Bid[a: .., b: .., c: .., d: .., e: .., f: .., g:
+		// .., h: .., ]
+		// Finished negotiation session in .*s
+		// SUMMARY TEXT LINE
+
+		int line = 0;
+
+		assertMatch("Starting .* session.*", listener.getLogs().get(line++));
+
+		int rounds = (listener.getLogs().size() - 7) / 4;
+		for (int n = 0; n < rounds; n++) {
+			// System.out.println("check round " + (n + 1));
+			assertMatch("Round " + (n + 1), listener.getLogs().get(line++));
+			assertBidOrAccept(" Turn 1: Boulware\\S* ",
+					listener.getLogs().get(line++));
+			assertBidOrAccept(" Turn 2: Conceder\\S* ",
+					listener.getLogs().get(line++));
+			assertBidOrAccept(" Turn 3: Random\\S* ",
+					listener.getLogs().get(line++));
+		}
+		// then the accept round
+		assertMatch("Round " + (rounds + 1), listener.getLogs().get(line++));
+		assertEquals(BidType.ACCEPT,
+				getType(" Turn 1: \\S* ", listener.getLogs().get(line++)));
+		assertEquals(BidType.ACCEPT,
+				getType(" Turn 2: \\S* ", listener.getLogs().get(line++)));
+		// then "found an agreement"
+		assertEquals(
+				BidType.PLAIN_BID,
+				getType(" Found an agreement: ", listener.getLogs().get(line++)));
+		assertMatch(" Finished negotiation session in \\S* ", listener
+				.getLogs().get(line++));
+		// list line is the wrap-up. Not checked yet, pretty complex.
+	}
+
 }
