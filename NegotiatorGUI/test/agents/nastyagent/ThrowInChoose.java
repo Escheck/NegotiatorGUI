dd547ea18f9d92953fa385b33163a02c8a4b67e2
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-07-22 10:56:54 +0000

    improved type checking for allowed actions

--- NegotiatorGUI/test/agents/nastyagent/ThrowInChoose.java
+++ NegotiatorGUI/test/agents/nastyagent/ThrowInChoose.java
@@ -7,7 +7,7 @@ import negotiator.actions.Action;
 public class ThrowInChoose extends NastyAgent {
 
 	@Override
-	public Action chooseAction(List<Class> possibleActions) {
+	public Action chooseAction(List<Class<? extends Action>> possibleActions) {
 		throw new RuntimeException("bla");
 	}
 }
