576d1378a5cfcb6ed5c6c22ad6bebd6e804eb646
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-07-22 08:42:09 +0000

    #1086 All agents only have access to TimeLineInterface. Fixed agents and superclasses to use TimeLineInterface. Fixed Forseti that called getUtilitySpace in its constructor (now in init).

--- test/agents/nastyagent/SleepInInit.java
+++ test/agents/nastyagent/SleepInInit.java
@@ -1,15 +1,16 @@
 package agents.nastyagent;
 
+import negotiator.AgentID;
 import negotiator.Deadline;
-import negotiator.session.Timeline;
+import negotiator.session.TimeLineInfo;
 import negotiator.utility.UtilitySpace;
 
 public class SleepInInit extends NastyAgent {
 
 	@Override
 	public void init(UtilitySpace utilitySpace, Deadline deadlines,
-			Timeline timeline, long randomSeed) {
-		super.init(utilitySpace, deadlines, timeline, randomSeed);
+			TimeLineInfo timeline, long randomSeed, AgentID id) {
+		super.init(utilitySpace, deadlines, timeline, randomSeed, id);
 		try {
 			Thread.sleep(2000000);// sleep 2000 seconds.
 		} catch (InterruptedException e) {
