2db2336671192a97ec5cd8b7dd0f35464242d682
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-07-27 12:44:30 +0000

    #1103 refactoring 	MultiTournamentConfiguration, preparing for XML serialization

--- src/negotiator/config/GuiConfiguration.java
+++ src/negotiator/config/GuiConfiguration.java
@@ -1,10 +1,6 @@
 package negotiator.config;
 
-import java.util.List;
-
-import negotiator.repository.MultiPartyProtocolRepItem;
-import negotiator.repository.PartyRepItem;
-import negotiator.repository.ProfileRepItem;
+import negotiator.protocol.MultilateralProtocol;
 
 /**
  * This is the configuration object used by the user interface. It stores all
@@ -13,21 +9,6 @@ import negotiator.repository.ProfileRepItem;
  * @author David Festen
  */
 public interface GuiConfiguration {
-
-	/**
-	 * Gets the mediator profile
-	 *
-	 * @return the mediator's ProfileRepItem or null if not existing
-	 */
-	public ProfileRepItem getMediatorProfile();
-
-	/**
-	 * Sets the mediator profile
-	 *
-	 * @param mediatorProfile
-	 *            the mediator's profile rep item or null if not existing
-	 */
-	public void setMediatorProfile(ProfileRepItem mediatorProfile);
 
 	/**
 	 * Gets the mediator index in the agent list
@@ -45,51 +26,6 @@ public interface GuiConfiguration {
 	void setMediatorIndex(int index);
 
 	/**
-	 * Gets the list of party repository items.
-	 *
-	 * @return a list of all chosen parties
-	 */
-	List<PartyRepItem> getPartyItems();
-
-	/**
-	 * Sets the list of chosen parties
-	 *
-	 * @param agents
-	 *            the list of all chosen parties
-	 */
-	void setPartyItems(List<PartyRepItem> agents);
-
-	/**
-	 * Gets the mediator
-	 *
-	 * @return the mediator or party or null if not set
-	 */
-	PartyRepItem getMediatorItem();
-
-	/**
-	 * Sets the mediator item
-	 *
-	 * @param mediatorItem
-	 *            the mediator
-	 */
-	void setMediatorItem(PartyRepItem mediatorItem);
-
-	/**
-	 * Gets the number of negotiation sessions to run
-	 *
-	 * @return the number of sessions
-	 */
-	int getNumSessions();
-
-	/**
-	 * Sets the number of negotiation sessions.
-	 *
-	 * @param numRounds
-	 *            the number of sessions
-	 */
-	void setNumSessions(int numRounds);
-
-	/**
 	 * Get the type of tournament
 	 *
 	 * @return the type of tournament
@@ -104,66 +40,6 @@ public interface GuiConfiguration {
 	 */
 	void setTournamentType(String type);
 
-	/**
-	 * Gets the protocol to run
-	 *
-	 * @return the protocol to run
-	 */
-	MultiPartyProtocolRepItem getProtocolItem();
-
-	/**
-	 * Sets the protocol to run.
-	 *
-	 * @param protocolItem
-	 *            the protocol to run
-	 */
-	void setProtocolItem(MultiPartyProtocolRepItem protocolItem);
-
-	/**
-	 * Gets the list of profiles used by the parties
-	 *
-	 * @return list of profiles used by the parties
-	 */
-	List<ProfileRepItem> getPartyProfileItems();
-
-	/**
-	 * Sets the list of profiles used by the parties
-	 *
-	 * @param partyProfileItems
-	 *            list of profiles used by the parties
-	 */
-	void setPartyProfileItems(List<ProfileRepItem> partyProfileItems);
-
-	/**
-	 * Gets the number of agents per session
-	 *
-	 * @return the number of agents per session
-	 */
-	int getNumAgentsPerSession();
-
-	/**
-	 * Sets the number of agents per session
-	 *
-	 * @param numAgents
-	 *            number of agents
-	 */
-	void setNumAgentsPerSession(int numAgents);
-
-	/**
-	 * Gets whether repetition is allowed when generating combinations of
-	 * agents.
-	 * 
-	 * @return true if allowed
-	 */
-	boolean getRepetitionAllowed();
-
-	/**
-	 * Sets whether repetition is allowed for generating sessions for the
-	 * current agent
-	 *
-	 * @param repetitionAllowed
-	 *            true if repetition is allowed
-	 */
-	void setRepetitionAllowed(boolean repetitionAllowed);
+	MultilateralProtocol getProtocol() throws Exception;
 
 }
--- src/negotiator/config/MultilateralTournamentConfiguration.java
+++ src/negotiator/config/MultilateralTournamentConfiguration.java
@@ -1,18 +1,99 @@
 package negotiator.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import negotiator.Deadline;
-import negotiator.protocol.MultilateralProtocol;
+import negotiator.repository.MultiPartyProtocolRepItem;
+import negotiator.repository.PartyRepItem;
+import negotiator.repository.ProfileRepItem;
 
 /**
- * Implementation of MultilateralTournamentConfigurationInterface
+ * Implementation of MultilateralTournamentConfigurationInterface. Extracted
+ * from {@link Configuration}.
  *
  * @author W.Pasman 27jul15
  */
+@XmlRootElement
 public class MultilateralTournamentConfiguration implements
 		MultilateralTournamentConfigurationInterface {
 
+	@XmlAttribute
 	private Deadline deadline;
-	private MultilateralProtocol protocol;
+
+	/**
+	 * Holds the chosen protocol
+	 */
+	@XmlAttribute
+	private MultiPartyProtocolRepItem protocolItem;
+
+	/**
+	 * Holds the chosen mediator, if any
+	 */
+	@XmlAttribute
+	private PartyRepItem mediatorItem;
+
+	/**
+	 * Holds the list of chosen parties
+	 */
+	@XmlElementWrapper(name = "partyRepItems")
+	@XmlElement(name = "item")
+	private List<PartyRepItem> partyItems;
+
+	/**
+	 * Holds the list of chosen profiles
+	 */
+	@XmlElementWrapper(name = "partyProfileItems")
+	@XmlElement(name = "item")
+	private List<ProfileRepItem> partyProfileItems;
+
+	/**
+	 * Holds the number of session that should be run
+	 */
+	@XmlAttribute
+	private int numSessions;
+
+	/**
+	 * Holds the mediator profile if any
+	 */
+	@XmlAttribute
+	private ProfileRepItem mediatorProfile;
+
+	/**
+	 * Holds the number of agents per session
+	 */
+	@XmlAttribute
+	private int numberOfAgentsPerSession;
+
+	/**
+	 * Holds whether repetition is allowed or not;
+	 */
+	@XmlAttribute
+	private boolean repetitionAllowed;
+
+	public MultilateralTournamentConfiguration() {
+		partyItems = new ArrayList<PartyRepItem>();
+		partyProfileItems = new ArrayList<ProfileRepItem>();
+	}
+
+	public MultilateralTournamentConfiguration(Configuration config) {
+		setDeadline(config.getDeadline());
+		this.mediatorItem = config.getMediatorItem();
+		this.partyItems = new ArrayList<PartyRepItem>(config.getPartyItems());
+		this.partyProfileItems = new ArrayList<ProfileRepItem>(
+				config.getPartyProfileItems());
+		setProtocolItem(config.getProtocolItem());
+		this.protocolItem = config.getProtocolItem();
+		this.numSessions = config.getNumSessions();
+		this.mediatorProfile = config.getMediatorProfile();
+		this.numberOfAgentsPerSession = config.getNumAgentsPerSession();
+		this.repetitionAllowed = config.getRepetitionAllowed();
+	}
 
 	@Override
 	public Deadline getDeadline() {
@@ -23,26 +104,165 @@ public class MultilateralTournamentConfiguration implements
 		deadline = dl;
 	}
 
+	/**
+	 * Gets the list of party repository items.
+	 *
+	 * @return a list of all chosen parties
+	 */
 	@Override
-	public MultilateralProtocol getProtocol() throws Exception {
-		return protocol;
+	public List<PartyRepItem> getPartyItems() {
+		return partyItems;
 	}
 
+	/**
+	 * Sets the list of chosen parties
+	 *
+	 * @param agents
+	 *            the list of all chosen parties
+	 */
 	@Override
-	public void setProtocol(MultilateralProtocol newProtocol) {
-		protocol = newProtocol;
+	public void setPartyItems(List<PartyRepItem> agents) {
+		this.partyItems = agents;
 	}
 
+	/**
+	 * Gets the number of negotiation sessions to run
+	 *
+	 * @return the number of sessions
+	 */
+	@Override
+	public int getNumSessions() {
+		return numSessions;
+	}
+
+	/**
+	 * Sets the number of negotiation sessions.
+	 *
+	 * @param numSessions
+	 *            the number of sessions
+	 */
+	@Override
+	public void setNumSessions(int numSessions) {
+		this.numSessions = numSessions;
+	}
+
+	/**
+	 * Gets the mediator
+	 *
+	 * @return the mediator or party or null if not set
+	 */
+	@Override
+	public PartyRepItem getMediatorItem() {
+		return mediatorItem;
+	}
+
+	/**
+	 * Sets the mediator item
+	 *
+	 * @param mediatorItem
+	 *            the mediator
+	 */
+	@Override
+	public void setMediatorItem(PartyRepItem mediatorItem) {
+		this.mediatorItem = mediatorItem;
+	}
+
+	/**
+	 * Gets the number of negotiation sessions to run
+	 *
+	 * @return the number of sessions
+	 */
 	@Override
 	public int getNumTournaments() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numSessions;
+	}
+
+	/**
+	 * Gets the protocol to run
+	 *
+	 * @return the protocol to run
+	 */
+	@Override
+	public MultiPartyProtocolRepItem getProtocolItem() {
+		return protocolItem;
 	}
 
 	@Override
+	public void setProtocolItem(MultiPartyProtocolRepItem item) {
+		protocolItem = item;
+	}
+
+	/**
+	 * Gets the list of profiles used by the parties
+	 *
+	 * @return list of profiles used by the parties
+	 */
+	@Override
+	public List<ProfileRepItem> getPartyProfileItems() {
+		return partyProfileItems;
+	}
+
+	/**
+	 * Sets the list of profiles used by the parties
+	 *
+	 * @param partyProfileItems
+	 *            list of profiles used by the parties
+	 */
+	@Override
+	public void setPartyProfileItems(List<ProfileRepItem> partyProfileItems) {
+		this.partyProfileItems = partyProfileItems;
+	}
+
+	/**
+	 * Gets the number of agents per session
+	 *
+	 * @return the number of agents per session
+	 */
+	@Override
 	public int getNumAgentsPerSession() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numberOfAgentsPerSession;
+	}
+
+	/**
+	 * Sets the number of agents per session
+	 *
+	 * @param numAgents
+	 *            number of agents
+	 */
+	@Override
+	public void setNumAgentsPerSession(int numAgents) {
+		numberOfAgentsPerSession = numAgents;
+	}
+
+	/**
+	 * Gets whether repetition is allowed when generating combinations of
+	 * agents.
+	 *
+	 * @return true if allowed
+	 */
+	@Override
+	public boolean getRepetitionAllowed() {
+		return repetitionAllowed;
+	}
+
+	/**
+	 * Sets whether repetition is allowed for generating sessions for the
+	 * current agent
+	 *
+	 * @param repetitionAllowed
+	 *            true if repetition is allowed
+	 */
+	@Override
+	public void setRepetitionAllowed(boolean repetitionAllowed) {
+		this.repetitionAllowed = repetitionAllowed;
+	}
+
+	public ProfileRepItem getMediatorProfile() {
+		return mediatorProfile;
+	}
+
+	public void setMediatorProfile(ProfileRepItem mediatorProfile) {
+		this.mediatorProfile = mediatorProfile;
 	}
 
 }
