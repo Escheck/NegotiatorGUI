bc34663462e4325853b0dcd06c688715cf2597ac
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-10-22 12:37:04 +0000

    #1128 values now are final

--- src/negotiator/issue/ValueReal.java
+++ src/negotiator/issue/ValueReal.java
@@ -15,38 +15,34 @@ public class ValueReal extends Value {
 	private static final long serialVersionUID = 8695408707220542052L;
 	@XmlAttribute
 	private double value;
-	
-	public ValueReal() { }
-	
+
+	public ValueReal() {
+	}
+
 	public ValueReal(double r) {
 		value = r;
 	}
-	
+
 	public ISSUETYPE getType() {
 		return ISSUETYPE.REAL;
 	}
-	
+
 	public double getValue() {
 		return value;
 	}
-	
-	public void setValue(double value) {
-		this.value = value;
-	}
-	
+
 	public String toString() {
 		return Double.toString(value);
 	}
-	
+
 	public boolean equals(Object pObject) {
-		if(pObject instanceof ValueReal) {
-			ValueReal val = (ValueReal)pObject;
-			return  value==val.getValue();
-		} else 
-			if(pObject instanceof Double){
-				double val = (Double) pObject;
-				return value == val;
-			} else
+		if (pObject instanceof ValueReal) {
+			ValueReal val = (ValueReal) pObject;
+			return value == val.getValue();
+		} else if (pObject instanceof Double) {
+			double val = (Double) pObject;
+			return value == val;
+		} else
 			return false;
 	}
 }
