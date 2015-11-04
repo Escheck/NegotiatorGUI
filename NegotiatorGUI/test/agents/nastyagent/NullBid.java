a59d5d82bea7aa7b686dc39afeeec991ea0cb333
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-11-04 15:09:30 +0000

    #1153 added NullBid agent to show new issue with Offer

---
+++ NegotiatorGUI/test/agents/nastyagent/NullBid.java
@@ -0,0 +1,21 @@
+package agents.nastyagent;
+
+import java.util.List;
+
+import negotiator.actions.Action;
+import negotiator.actions.Offer;
+
+/**
+ * returns a deliberately miscrafted bid that contains an integer value where a
+ * discrete is expected, or the other way round. The idea is that the opponent
+ * may call getUtility on it and then cause a class cast exception.
+ * 
+ * @author W.Pasman 2nov15
+ *
+ */
+public class NullBid extends NastyAgent {
+	@Override
+	public Action chooseAction(List<Class<? extends Action>> possibleActions) {
+		return new Offer(null);
+	}
+}
