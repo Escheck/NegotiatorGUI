ad53ca980a38a9fbeb9c00b50c1181be8f334428
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2015-11-19 13:43:19 +0000

    #1153 added check for bid=null in constructor of offer

--- NegotiatorGUI/src/negotiator/actions/Offer.java
+++ NegotiatorGUI/src/negotiator/actions/Offer.java
@@ -12,41 +12,53 @@ import negotiator.Bid;
 public class Offer extends Action {
 
 	/** Bid to be offered to the opponent. */
-    protected Bid bid;
-    
-    /**
-     * Creates an action symbolizing an offer for the opponent.
-     * @param agentID id of the agent which created the offer.
-     * @param bid for the opponent.
-     */
-    public Offer(AgentID agentID, Bid bid) {
+	protected Bid bid;
+
+	/**
+	 * Creates an action symbolizing an offer for the opponent.
+	 * 
+	 * @param agentID
+	 *            id of the agent which created the offer.
+	 * @param bid
+	 *            for the opponent.
+	 */
+	public Offer(AgentID agentID, Bid bid) {
 		super(agentID);
-        this.bid = bid;
-    }
-    
-    /**
-     * Creates an action symbolizing an offer for the opponent.
-     * @param bid for the opponent.
-     */
-    public Offer(Bid bid) {
-        this.bid = bid;
-    }
-    
-    /**
-     * @return hashcode of this object.
-     */
-    @Override
+		if (bid == null) {
+			throw new NullPointerException("offer with null bid");
+		}
+		this.bid = bid;
+	}
+
+	/**
+	 * Creates an action symbolizing an offer for the opponent.
+	 * 
+	 * @param bid
+	 *            for the opponent.
+	 */
+	public Offer(Bid bid) {
+		if (bid == null) {
+			throw new NullPointerException("offer with null bid");
+		}
+		this.bid = bid;
+	}
+
+	/**
+	 * @return hashcode of this object.
+	 */
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((bid == null) ? 0 : bid.hashCode());
 		return result;
 	}
-    
-    /**
-     * @param obj object to which this object is compared.
-     * @return true if this object is equal to the given object.
-     */
+
+	/**
+	 * @param obj
+	 *            object to which this object is compared.
+	 * @return true if this object is equal to the given object.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -63,31 +75,32 @@ public class Offer extends Action {
 			return false;
 		return true;
 	}
-	
+
 	/**
 	 * Creates an action symbolizing an offer to the opponent.
 	 * 
-	 * @param agent which created this offer.
-	 * @param bid to be offered to the opponent.
+	 * @param agent
+	 *            which created this offer.
+	 * @param bid
+	 *            to be offered to the opponent.
 	 */
-    public Offer(Agent agent, Bid bid) {
-        this(agent.getAgentID(), bid);
-    }
-    
-    /**
-     * Returns the bid offered by the agent which
-     * created this offer.
-     * 
-     * @return bid to offer.
-     */
-    public Bid getBid() {
-        return bid;
-    }
-    
-    /**
-     * @return string representation of action: "(Offer: BID)".
-     */
-    public String toString() {
-        return "(Offer: " + (bid == null ? "null" : bid.toString()) + ")";
-    }
+	public Offer(Agent agent, Bid bid) {
+		this(agent.getAgentID(), bid);
+	}
+
+	/**
+	 * Returns the bid offered by the agent which created this offer.
+	 * 
+	 * @return bid to offer.
+	 */
+	public Bid getBid() {
+		return bid;
+	}
+
+	/**
+	 * @return string representation of action: "(Offer: BID)".
+	 */
+	public String toString() {
+		return "(Offer: " + (bid == null ? "null" : bid.toString()) + ")";
+	}
 }
