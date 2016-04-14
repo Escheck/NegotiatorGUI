12a97909f0dfbfaf21d84013c723e009d25b1abb
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2016-04-14 09:46:39 +0000

    #1239 javadoc of multi session ended event.

--- src/negotiator/events/MultipartySessionEndedEvent.java
+++ src/negotiator/events/MultipartySessionEndedEvent.java
@@ -1,29 +1,34 @@
 package negotiator.events;
 
-
 import negotiator.Bid;
 import negotiator.session.Session;
 
-public class MultipartySessionEndedEvent extends NegotiationEvent
-{
-    private Session session;
-    private Bid agreement;
+/**
+ * Records an end of a multi-party session end.
+ *
+ */
+@SuppressWarnings("serial")
+public class MultipartySessionEndedEvent extends NegotiationEvent {
+	private Session session;
+	private Bid agreement;
 
-    public MultipartySessionEndedEvent(Object source, Session session, Bid agreement)
-    {
+	public MultipartySessionEndedEvent(Object source, Session session,
+			Bid agreement) {
 
-        super(source);
-        this.session = session;
-        this.agreement = agreement;
-    }
+		super(source);
+		this.session = session;
+		this.agreement = agreement;
+	}
 
-    public Session getSession()
-    {
-        return session;
-    }
+	public Session getSession() {
+		return session;
+	}
 
-    public Bid getAgreement()
-    {
-        return agreement;
-    }
+	/**
+	 * 
+	 * @return final agreement bid, or null if no agreement was reached
+	 */
+	public Bid getAgreement() {
+		return agreement;
+	}
 }
