b9d7debf088d8ad77fc01c8c7c3710d77e3ec39b
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2016-04-19 10:06:34 +0000

    #1243 removed all code, this project is now only assembling the release components.

--- src/negotiator/Vote.java
+++
@@ -1,5 +0,0 @@
-package negotiator;
-
-public enum Vote {
-	REJECT,ACCEPT;	
-}
--- src/parties/in4010/q12015/group13/IssueModel.java
+++
@@ -1,9 +0,0 @@
-package parties.in4010.q12015.group13;
-
-import negotiator.issue.Value;
-
-public interface IssueModel<V extends Value> {
-	public void addBid(V v);
-
-	public double estimateUtility(V v);
-}
