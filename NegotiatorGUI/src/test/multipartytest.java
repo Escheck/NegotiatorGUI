3da4c77a039f35d991f430efbf7d02af55c08d4b
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2016-05-18 09:26:21 +0000

    #1257 added more complex test that ignores columns that are not exactly the same every time

--- NegotiatorGUI/src/test/multipartytest.java
+++ NegotiatorGUI/src/test/multipartytest.java
@@ -1,11 +1,11 @@
 package test;
 
-import static org.junit.Assert.assertTrue;
-
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.xml.bind.JAXBException;
 
@@ -16,19 +16,73 @@ import org.junit.Test;
 public class multipartytest {
 
 	@Test
-	public void test1() throws JAXBException, IOException {
+	public void test1() throws IOException, NotEqualException, JAXBException {
 		File output = File.createTempFile("temp-file-name", ".tmp");
 
 		Runner.main(new String[] { "test/multitest.xml",
 				output.getCanonicalPath() });
-		assertTrue("result differs from expected",
-				equal(output, new File("test/multitestexpected.txt")));
+		equal(output, new File("test/multitestexpected.txt"));
 	}
 
-	private boolean equal(File file1, File file2) throws IOException {
-		byte[] f1 = Files.readAllBytes(file1.toPath());
-		byte[] f2 = Files.readAllBytes(file2.toPath());
-		return Arrays.equals(f1, f2);
+	/**
+	 * Check the output log files line by line.
+	 * 
+	 * @param file1
+	 * @param file2
+	 * @return
+	 * @throws IOException
+	 * @throws NotEqualException
+	 */
+	private void equal(File file1, File file2) throws IOException,
+			NotEqualException {
+		BufferedReader br1 = new BufferedReader(new FileReader(file1));
+		BufferedReader br2 = new BufferedReader(new FileReader(file2));
+
+		int linenr = 1;
+		String line1, line2;
+		while ((line1 = br1.readLine()) != null) {
+			line2 = br2.readLine();
+			equal(linenr, line1, line2);
+			linenr++;
+		}
+
+		br1.close();
+		br2.close();
 	}
 
+	/**
+	 * ignore run time(s), agent1, agent2, agent3. We ignore the agents because
+	 * they contain memory address
+	 */
+	List<Integer> indicesToSkip = Arrays.asList(0, 12, 13, 14);
+
+	/**
+	 * Check that given lines are equal. ";" is separator character.
+	 * 
+	 * @param linenr
+	 * @param line1
+	 * @param line2
+	 * @throws NotEqualException
+	 */
+
+	private void equal(int linenr, String line1, String line2)
+			throws NotEqualException {
+		String[] elements1 = line1.split(";");
+		String[] elements2 = line2.split(";");
+		if (elements1.length != elements2.length) {
+			throw new NotEqualException(
+					"Lines do not contain the same number of results", linenr,
+					"" + elements1.length, "" + elements2.length);
+		}
+		for (int i = 0; i < elements1.length; i++) {
+			if (indicesToSkip.contains(i))
+				continue;
+			String elem1 = elements1[i];
+			String elem2 = elements2[i];
+			if (!elem1.equals(elem2)) {
+				throw new NotEqualException("element " + (i + 1)
+						+ " is not equal", linenr, elem1, elem2);
+			}
+		}
+	}
 }
