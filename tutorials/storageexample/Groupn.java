7e7fbf2c68fa09d0dabda6b3cdd4fa9a5c2b69e1
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2016-12-15 10:28:08 +0000

    #1376 modified storage example

--- tutorials/storageexample/Groupn.java
+++ tutorials/storageexample/Groupn.java
@@ -1,10 +1,7 @@
 package storageexample;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import list.Tuple;
 import negotiator.AgentID;
 import negotiator.Bid;
 import negotiator.Deadline;
@@ -14,7 +11,6 @@ import negotiator.actions.Offer;
 import negotiator.parties.AbstractNegotiationParty;
 import negotiator.persistent.PersistentDataContainer;
 import negotiator.persistent.PersistentDataType;
-import negotiator.persistent.StandardInfo;
 import negotiator.persistent.StandardInfoList;
 import negotiator.timeline.TimeLineInfo;
 import negotiator.utility.AbstractUtilitySpace;
@@ -22,11 +18,10 @@ import negotiator.utility.AbstractUtilitySpace;
 /**
  * This is your negotiation party.
  */
-public class GroupX extends AbstractNegotiationParty {
+public class Groupn extends AbstractNegotiationParty {
 
 	private Bid lastReceivedBid = null;
 	private int nrChosenActions = 0; // number of times chosenAction was called.
-	private StandardInfoList history;
 
 	@Override
 	public void init(AbstractUtilitySpace utilSpace, Deadline dl, TimeLineInfo tl, long randomSeed, AgentID agentId,
@@ -42,20 +37,6 @@ public class GroupX extends AbstractNegotiationParty {
 		if (getData().getPersistentDataType() != PersistentDataType.STANDARD) {
 			throw new IllegalStateException("need standard persistent data");
 		}
-		history = (StandardInfoList) getData().get();
-
-		if (!history.isEmpty()) {
-			// example of using the history. Compute for each party the maximum
-			// utility of the bids in last session.
-			Map<String, Double> maxutils = new HashMap<String, Double>();
-			StandardInfo lastinfo = history.get(history.size() - 1);
-			for (Tuple<String, Double> offered : lastinfo.getUtilities()) {
-				String party = offered.get1();
-				Double util = offered.get2();
-				maxutils.put(party, maxutils.containsKey(party) ? Math.max(maxutils.get(party), util) : util);
-			}
-			System.out.println(maxutils); // notice tournament suppresses all output.
-		}
 	}
 
 	/**
@@ -70,6 +51,8 @@ public class GroupX extends AbstractNegotiationParty {
 	@Override
 	public Action chooseAction(List<Class<? extends Action>> validActions) {
 		nrChosenActions++;
+
+		StandardInfoList history = (StandardInfoList) getData().get();
 		if (nrChosenActions > history.size() & lastReceivedBid != null) {
 			return new Accept(getPartyId(), lastReceivedBid);
 		} else {
