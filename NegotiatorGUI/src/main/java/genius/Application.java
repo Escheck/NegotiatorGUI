02294256d50825bcb8ed5fdb904d4677520f3037
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2018-02-07 10:35:33 +0000

    #1509 NegotiatorGUI main class now genius/Application. This installs the repositories on proper locations at startup. Using the new maven-built dependencies now.

--- NegotiatorGUI/src/main/java/genius/Application.java
+++ NegotiatorGUI/src/main/java/genius/Application.java
@@ -1,8 +1,15 @@
+package genius;
+
+import java.io.IOException;
+
+import genius.domains.DomainInstaller;
 import negotiator.gui.NegoGUIApp;
 
 public class Application {
-	public static void main(String[] args ) {
+	public static void main(String[] args) throws IOException {
+		ProtocolsInstaller.run();
 		DomainInstaller.run();
+		AgentsInstaller.run();
 		NegoGUIApp.run(args);
 	}
 }
