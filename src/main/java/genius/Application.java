e20669661e9cf67a058a4c6f24df6248398a8119
wouter <wouter@906596ab-7e2f-0410-9379-aac80b4bceee>
2018-03-07 09:52:24 +0000

    #1564 update title and authors. Fixed Assembly plugin issue

--- src/main/java/genius/Application.java
+++ src/main/java/genius/Application.java
@@ -3,13 +3,13 @@ package genius;
 import java.io.IOException;
 
 import genius.domains.DomainInstaller;
-import negotiator.gui.DeprecatedOldMain;
+import negotiator.gui.NegoGUIApp;
 
 public class Application {
 	public static void main(String[] args) throws IOException {
 		ProtocolsInstaller.run();
 		DomainInstaller.run();
 		AgentsInstaller.run();
-		DeprecatedOldMain.run(args);
+		NegoGUIApp.run(args);
 	}
 }
